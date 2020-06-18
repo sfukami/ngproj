b43fed2a424925e8b235f320344b71cd7d702571
sfukami <xxx@dummy.com>
2020-06-19 00:30:55 +0900

    デバッグログファイル追加

---
+++ ngLibCore/debug/log/ngDebugLog.cpp
@@ -0,0 +1,76 @@
+﻿/*!
+* @file		ngDebugLog.cpp
+* @brief	デバッグログ出力
+* @date		2018-10-21
+* @author	s.fukami
+*/
+
+#include <time.h>
+#include "ngLibCore/common/ngCommon.h"
+#include "ngDebugLog.h"
+
+namespace ng
+{
+	CDebugLog::CDebugLog()
+	{
+	}
+	CDebugLog::~CDebugLog()
+	{
+	}
+
+	NG_ERRCODE CDebugLog::Open(const char* pFilename)
+	{
+		NG_ERRCODE ret = eNG_E_FAIL;
+
+		if(NG_SUCCEEDED(ret = m_file.Open(pFilename, "w"))) {
+			NG_DPRINTF("Debug log file open succeeded. file:%s\n", pFilename);
+		} else {
+			NG_ERRLOG(ret, "Debug log file open failed. file:%s", pFilename);
+		}
+
+		return ret;
+	}
+
+	void CDebugLog::Write(const char* pStr)
+	{
+		struct tm newTime;
+		if(_getLocalTime(&newTime) == 0) {
+			char buf[32];
+			::asctime_s(buf, sizeof(buf), &newTime);
+			m_file.Write("[%.19s] %s\n", buf, pStr);
+			/*
+			m_file.Write(
+				"[%d02d月 %02d日 - %02d:%02d:%02d] %s", 
+				newTime->tm_mon + 1, newTime->tm_mday, newTime->tm_hour, newTime->tm_min, newTime->tm_sec, pStr
+				);
+			*/
+		} else {
+			m_file.Write(pStr);
+		}
+	}
+	void CDebugLog::Write(const wchar_t* pStr)
+	{
+		struct tm newTime;
+		if(_getLocalTime(&newTime) == 0) {
+			wchar_t buf[32];
+			::_wasctime_s(buf, sizeof(buf), &newTime);
+			m_file.Write(L"[%.19s] %s\n", buf, pStr);
+		} else {
+			m_file.Write(pStr);
+		}
+	}
+
+	void CDebugLog::Close()
+	{
+		m_file.Close();
+	}
+
+	errno_t CDebugLog::_getLocalTime(struct tm* pTime)
+	{
+		__time64_t longTime;
+		::_time64(&longTime);
+
+		return ::_localtime64_s(pTime, &longTime);
+	}
+
+}	// namespace ng
---
+++ ngLibCore/debug/log/ngDebugLog.h
@@ -0,0 +1,59 @@
+﻿/*!
+* @file		ngDebugLog.h
+* @brief	デバッグログ出力
+* @date		2018-10-21
+* @author	s.fukami
+*/
+
+#ifndef __NG_CORE_DEBUG_LOG_H__
+#define __NG_CORE_DEBUG_LOG_H__
+
+#include "ngLibCore/io/file/ngFile.h"
+
+namespace ng
+{
+	/*!
+	* デバッグログ出力
+	*/
+	NG_DECL class CDebugLog
+	{
+	public:
+		CDebugLog();
+		~CDebugLog();
+
+		/*!
+		* @brief					ログ出力先ファイルオープン
+		* @param pFilename			ログ出力先ファイル名
+		* @return					NG エラーコード
+		*/
+		NG_ERRCODE Open(const char* pFilename);
+
+		/*!
+		* @brief					ログ書き込み
+		* @param pStr				
+		*/
+		void Write(const char* pStr);
+		void Write(const wchar_t* pStr);
+
+		/*!
+		* @brief					ログ出力先ファイルクローズ
+		*/
+		void Close();
+
+		/*! ファイルをオープンしているか */
+		bool IsOpen() const { return m_file.IsOpen(); }
+
+	private:
+		/*!
+		* @brief					現在時刻取得
+		* @param tm					現在時刻格納先
+		*/
+		errno_t _getLocalTime(struct tm* pTime);
+
+	private:
+		CFile m_file;	//!< ファイル入出力
+	};
+
+}	// namespace ng
+
+#endif	// __NG_CORE_DEBUG_LOG_H__
