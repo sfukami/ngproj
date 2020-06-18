b27cc1879e2d18a7f662a86380848502cce29afe
sfukami <xxx@dummy.com>
2020-06-19 00:09:13 +0900

    ルートメモリプール追加

---
+++ ngLibCore/memory/pool/ngRootMemoryPool.cpp
@@ -0,0 +1,56 @@
+﻿/*!
+* @file		ngRootMemoryPool.cpp
+* @brief	ルートメモリプール
+* @date		2019-01-10
+* @author	s.fukami
+*/
+
+#include "ngLibCore/common/ngCommon.h"
+#include "ngRootMemoryPool.h"
+
+namespace ng
+{
+	CRootMemoryPool::CRootMemoryPool()
+	{
+	}
+
+	CRootMemoryPool::~CRootMemoryPool()
+	{
+		Finalize();
+	}
+
+	NG_ERRCODE CRootMemoryPool::Initialize(size_type size)
+	{
+		NG_ERRCODE ret = eNG_S_OK;
+
+		if(NG_FAILED(ret = m_rootAlloc.Initialize("ng_rootmem"))) {
+			NG_ERRLOG(ret, "ルートアロケータの初期化に失敗");
+			return ret;
+		}
+		if(NG_FAILED(ret = m_memPool.Pool(NG_NEW_BYTE(m_rootAlloc, size), size))) {
+			NG_ERRLOG(ret, "ルートアロケータを用いたメモリ領域のプールに失敗");
+			return ret;
+		}
+
+		return ret;
+	}
+
+	void CRootMemoryPool::Finalize()
+	{
+		if(m_memPool.IsPool()) {
+			NG_DELETE(m_rootAlloc, m_memPool.GetMemory());
+			m_memPool.Unpool();
+		}
+	}
+
+	CMemoryPool& CRootMemoryPool::GetMemoryPool()
+	{
+		return m_memPool;
+	}
+
+	const CMemoryPool& CRootMemoryPool::GetMemoryPool() const
+	{
+		return m_memPool;
+	}
+
+}	// namespace ng
---
+++ ngLibCore/memory/pool/ngRootMemoryPool.h
@@ -0,0 +1,53 @@
+﻿/*!
+* @file		ngRootMemoryPool.h
+* @brief	ルートメモリプール
+* @date		2019-01-10
+* @author	s.fukami
+*/
+
+#ifndef __NG_ROOT_MEMORY_POOL_H__
+#define __NG_ROOT_MEMORY_POOL_H__
+
+#include "ngLibCore/memory/pool/ngMemoryPool.h"
+#include "ngLibCore/allocator/ngDefaultAllocator.h"
+
+namespace ng
+{
+	/*!
+	* ルートメモリプール
+	*/
+	class NG_DECL CRootMemoryPool
+	{
+	public:
+		/*! コンストラクタ */
+		CRootMemoryPool();
+
+		/*! デストラクタ */
+		virtual ~CRootMemoryPool();
+
+		/*!
+		* @brief					初期化
+		* @param size				メモリサイズ
+		* @return					NG エラーコード
+		*/
+		NG_ERRCODE Initialize(size_type size);
+
+		/*!
+		* @brief					終了処理
+		*/
+		void Finalize();
+
+		/*!
+		* @brief					メモリプール取得
+		*/
+		CMemoryPool& GetMemoryPool();
+		const CMemoryPool& GetMemoryPool() const;
+
+	private:
+		CDefaultAllocator m_rootAlloc;	//!< ルートアロケータ
+		CMemoryPool m_memPool;		//!< メモリプール
+	};
+
+}	// namespace ng
+
+#endif	// __NG_ROOT_MEMORY_POOL_H__
