0e07da20f0ebfcb553a8df568f0e344ce6a78c25
sfukami <xxx@dummy.com>
2020-06-19 00:23:35 +0900

    シングルトンクラス追加

---
+++ ngLibCore/traits/ngNonCopyable.h
@@ -0,0 +1,56 @@
+﻿/*!
+* @file		ngNonCopyable.h
+* @brief	コピー禁止
+* @date		2014-07-07
+* @author	s.fukami
+*/
+// 更新履歴
+//	2013-05-25
+//	テンプレートクラスに変更（CRTP）
+//	多重継承の際の「空の基底クラスに対する最適化」を行うため
+
+#ifndef __NG_CORE_NONCOPYABLE_H__
+#define __NG_CORE_NONCOPYABLE_H__
+
+namespace ng
+{
+	/*!
+	* @brief					派生クラスの代入・コピー禁止クラス
+	* @note						継承して使用する。継承はprivateで行うこと
+	* @attention				派生クラスでコピーコンストラクタ等を実装することでコピーが可能になるので注意
+	*/
+	template <class T>
+	class NG_DECL CNonCopyable
+	{
+	protected:
+		/*!
+		* @brief					コンストラクタ
+		* @note						このクラス単体では使用しないのでprotected
+		*/
+		CNonCopyable() { }
+
+		/*!
+		* @brief					デストラクタ
+		* @node						ポリモーフィズムは禁止するのでprotected。virtualも不要
+		*/
+		~CNonCopyable() { }
+
+	private:
+		/*!
+		* @brief					コピーコンストラクタ
+		* @note						呼び出し禁止のためprivate宣言
+		*							実装の必要無し
+		*/
+		CNonCopyable(const CNonCopyable&);
+
+		/*!
+		* @brief					代入演算子のオーバーロード
+		* @note						呼び出し禁止のためprivate宣言
+		* 							実装の必要無し
+		*/
+		CNonCopyable& operator=(const CNonCopyable&);
+	};
+
+}	// namespace ng
+
+#endif	// __NG_CORE_NONCOPYABLE_H__
---
+++ ngLibCore/traits/ngSingleton.h
@@ -0,0 +1,109 @@
+﻿/*!
+* @file		ngSingleton.h
+* @brief	シングルトン
+* @date		2015-02-20
+* @author	s.fukami
+*/
+
+#ifndef __NG_CORE_SINGLETON_H__
+#define __NG_CORE_SINGLETON_H__
+
+#include "ngLibCore/common/debug/ngDbgMacro.h"
+#include "ngNonCopyable.h"
+
+/*!
+* シングルトンクラス用定義
+*/
+#define NG_SINGLETON(_class)	friend class ng::CSingleton<_class>; \
+								private: \
+								static _class* _createInstance(); \
+								static void _destroyInstance(_class* pInst);
+								
+namespace ng
+{
+	/*!
+	* @brief					シングルトンクラス
+	* @tparam T					派生クラス（CRTP）
+	* @attention				派生クラスのコンストラクタをprivate指定すること
+	*							派生クラスにて次のメソッドを実装する必要がある
+	*							static T* _createInstance();		シングルトンインスタンスの生成を行う
+	*							static void _destroyInstance(T*);	シングルトンインスタンスの破棄を行う
+	*/
+	template <class T>
+	class NG_DECL CSingleton : private CNonCopyable<CSingleton<T> >
+	{
+	protected:
+		/*!
+		* @brief					コンストラクタ
+		* @note						このクラス単体では使用しないのでprotected
+		*/
+		CSingleton() { }
+
+		/*!
+		* @brief					デストラクタ
+		* @note						ポリモーフィズムは禁止するのでprotected
+		* 							及び、非仮想
+		*/
+		~CSingleton() { }
+
+	public:
+		/*!
+		* @brief					シングルトンインスタンス生成
+		*/
+		static void CreateInstance();
+
+		/*!
+		* @brief					シングルトンインスタンス破棄
+		*/
+		static void DestroyInstance();
+
+		/*!
+		* @brief					シングルトンインスタンス取得
+		*/
+		static T& GetInstance();
+
+	private:
+		static T* m_pInstance;	//!< シングルトンインスタンスへの参照
+	};
+
+	template <class T>
+	T* CSingleton<T>::m_pInstance = nullptr;
+
+	/*!
+	* @brief					シングルトンインスタンス生成
+	*/
+	template <class T>
+	void CSingleton<T>::CreateInstance()
+	{
+		if( ! m_pInstance)
+		{
+			// インスタンス生成
+			m_pInstance = T::_createInstance();
+			NG_ASSERT(m_pInstance);
+		}
+	}
+
+	/*!
+	* @brief					シングルトンインスタンス破棄
+	*/
+	template <class T>
+	void CSingleton<T>::DestroyInstance()
+	{
+		// インスタンス破棄
+		T::_destroyInstance(m_pInstance);
+		m_pInstance = nullptr;
+	}
+
+	/*!
+	* @brief					シングルトンインスタンス取得
+	*/
+	template <class T>
+	T& CSingleton<T>::GetInstance()
+	{
+		NG_ASSERT(m_pInstance);
+		return *m_pInstance;
+	}
+
+}	// namespace ng
+
+#endif	// __NG_CORE_SINGLETON_H__
