f970471aad5eb44b26df509970524a32e3a8e106
sfukami <xxx@dummy.com>
2020-06-23 14:47:57 +0900

    NGライブラリ コアシステム追加

---
+++ ngLibCore/system/ngCoreSystem.cpp
@@ -0,0 +1,60 @@
+﻿/*!
+* @file		ngCoreSystem.cpp
+* @brief	NGライブラリ コアシステム
+* @date		2018-02-12
+* @author	s.fukami
+*/
+
+#include "ngLibCore/common/ngCommon.h"
+#include "ngCoreSystem.h"
+
+namespace ng
+{
+	CCoreSystem CCoreSystem::m_instance;
+
+	CCoreSystem::CCoreSystem()
+	{
+	}
+
+	CCoreSystem::~CCoreSystem()
+	{
+	}
+
+	NG_ERRCODE CCoreSystem::Setup(const SetupParam& param)
+	{
+		NG_ERRCODE ret = eNG_S_OK;
+
+		// システムメモリ初期化
+		{
+			if(NG_FAILED(ret = m_sysMem.Initialize(param.sysMemInitParam))) {
+				NG_ERRLOG(ret, "システムメモリの初期化に失敗");
+				return ret;
+			}
+		}
+
+		return ret;
+	}
+
+	void CCoreSystem::Shutdown()
+	{
+		m_sysMem.Finalize();
+
+		CCoreSystem::DestroyInstance();
+	}
+
+	IMemoryAllocator* CCoreSystem::GetSysMemAlloc(eSystemMemoryType type)
+	{
+		return m_sysMem.GetAllocator(type);
+	}
+
+	CCoreSystem* CCoreSystem::_createInstance()
+	{
+		return &m_instance;
+	}
+
+	void CCoreSystem::_destroyInstance(CCoreSystem* pInst)
+	{
+		// 空定義
+	}
+
+}	// namespace ng
---
+++ ngLibCore/system/ngCoreSystem.h
@@ -0,0 +1,65 @@
+﻿/*!
+* @file		ngCoreSystem.h
+* @brief	NGライブラリ コアシステム
+* @date		2018-02-12
+* @author	s.fukami
+*/
+
+#ifndef __NG_CORE_CORE_SYSTEM_H__
+#define __NG_CORE_CORE_SYSTEM_H__
+
+#include "ngLibCore/traits/ngSingleton.h"
+#include "memory/ngSystemMemory.h"
+
+namespace ng
+{
+	/*!
+	* @brief					NGライブラリ コアシステム
+	*/
+	class NG_DECL CCoreSystem : public CSingleton<CCoreSystem>
+	{
+		NG_SINGLETON(CCoreSystem);
+
+	public:
+		// セットアップパラメータ
+		struct SetupParam
+		{
+			SetupParam();
+
+			CSystemMemory::InitParam sysMemInitParam;	//!< システムメモリ 初期化パラメータ
+		};
+
+	private:
+		CCoreSystem();
+		virtual ~CCoreSystem();
+
+	public:
+		/*!
+		* @brief					セットアップ
+		* @param param				セットアップパラメータ
+		* @return					NG エラーコード
+		*/
+		NG_ERRCODE Setup(const SetupParam& param);
+
+		/*!
+		* @brief					終了処理
+		*/
+		void Shutdown();
+
+		/*!
+		* @brief					システムメモリアロケータ取得
+		* @param type				システムメモリタイプ
+		* @return					メモリアロケータ
+		*/
+		IMemoryAllocator* GetSysMemAlloc(eSystemMemoryType type);
+
+	private:
+		static CCoreSystem m_instance;	//!< コアシステム インスタンス
+
+	private:
+		CSystemMemory m_sysMem;	//!< システムメモリ
+	};
+
+}	// namespace ng
+
+#endif	// __NG_CORE_CORE_SYSTEM_H__
