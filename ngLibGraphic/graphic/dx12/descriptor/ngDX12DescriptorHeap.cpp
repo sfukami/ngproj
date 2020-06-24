f2409c5a90bec1ffdcfc052d8500432fbe26c359
sfukami <xxx@dummy.com>
2020-06-24 17:58:15 +0900

    DX12ディスクリプタヒープ追加

---
+++ ngLibGraphic/graphic/dx12/descriptor/ngDX12DescriptorHeap.cpp
@@ -0,0 +1,108 @@
+﻿/*!
+* @file		ngDX12DescriptorHeap.cpp
+* @brief	DX12ディスクリプタヒープ
+* @date		2017-09-19
+* @author	s.fukami
+*/
+
+#include "ngLibCore/common/ngCommon.h"
+#include "../common/ngDX12Common.h"
+#include "../device/ngDX12Device.h"
+#include "ngDX12DescriptorHeap.h"
+
+namespace ng
+{
+	CDX12DescriptorHeap::CDX12DescriptorHeap()
+		: m_pIDescriptorHeap(nullptr)
+		, m_incrementSize(0)
+	{
+	}
+
+	CDX12DescriptorHeap::~CDX12DescriptorHeap()
+	{
+		Destroy();
+	}
+
+	NG_ERRCODE CDX12DescriptorHeap::Create(
+		CDX12Device& device,
+		D3D12_DESCRIPTOR_HEAP_TYPE type,
+		u32 descriptorNum,
+		D3D12_DESCRIPTOR_HEAP_FLAGS flags
+		)
+	{
+		NG_ERRCODE ret = NG_ERRCODE_DEFAULT;
+		ID3D12Device* pIDevice = device.Interface();
+
+		D3D12_DESCRIPTOR_HEAP_DESC desc = {};
+		desc.Type = type;
+		desc.NumDescriptors = descriptorNum;
+		desc.Flags = flags;
+		//desc.NodeMask = 
+
+		// DX12ディスクリプタヒープ生成
+		if(NG_FAILED(ret = pIDevice->CreateDescriptorHeap(
+			&desc,
+			IID_PPV_ARGS(&m_pIDescriptorHeap)
+			))) {
+			NG_DXERRLOG("DX12DescriptorHeap", ret, CreateDescriptorHeap, "DX12ディスクリプタヒープの作成に失敗しました.");
+		}
+
+		m_incrementSize = pIDevice->GetDescriptorHandleIncrementSize(type);
+
+		return ret;
+	}
+
+	void CDX12DescriptorHeap::Destroy()
+	{
+		NG_SAFE_RELEASE(m_pIDescriptorHeap);
+	}
+
+	bool CDX12DescriptorHeap::IsValid() const
+	{
+		return (m_pIDescriptorHeap != nullptr);
+	}
+
+	D3D12_CPU_DESCRIPTOR_HANDLE CDX12DescriptorHeap::GetCPUDescriptorHandleForHeapStart() const
+	{
+		if(!IsValid()) {
+			return D3D12_CPU_DESCRIPTOR_HANDLE();
+		}
+
+		return m_pIDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+	}
+
+	CD3DX12_CPU_DESCRIPTOR_HANDLE CDX12DescriptorHeap::GetCPUDescriptorHandle(int index) const
+	{
+		return CD3DX12_CPU_DESCRIPTOR_HANDLE(
+			GetCPUDescriptorHandleForHeapStart(),
+			index, m_incrementSize
+			);
+	}
+
+	D3D12_GPU_DESCRIPTOR_HANDLE CDX12DescriptorHeap::GetGPUDescriptorHandleForHeapStart() const
+	{
+		if(!IsValid()) {
+			return D3D12_GPU_DESCRIPTOR_HANDLE();
+		}
+
+		return m_pIDescriptorHeap->GetGPUDescriptorHandleForHeapStart();
+	}
+
+	CD3DX12_GPU_DESCRIPTOR_HANDLE CDX12DescriptorHeap::GetGPUDescriptorHandle(int index) const
+	{
+		return CD3DX12_GPU_DESCRIPTOR_HANDLE(
+			GetGPUDescriptorHandleForHeapStart(),
+			index, m_incrementSize
+			);
+	}
+
+	ID3D12DescriptorHeap* CDX12DescriptorHeap::Interface()
+	{
+		return m_pIDescriptorHeap;
+	}
+	const ID3D12DescriptorHeap* CDX12DescriptorHeap::Interface() const
+	{
+		return m_pIDescriptorHeap;
+	}
+
+}	// namespace ng
---
+++ ngLibGraphic/graphic/dx12/descriptor/ngDX12DescriptorHeap.h
@@ -0,0 +1,98 @@
+﻿/*!
+* @file		ngDX12DescriptorHeap.h
+* @brief	DX12ディスクリプタヒープ
+* @date		2017-09-19
+* @author	s.fukami
+*/
+
+#ifndef __NG_GRAPHIC_DX12_DESCRIPTOR_HEAP_H__
+#define __NG_GRAPHIC_DX12_DESCRIPTOR_HEAP_H__
+
+#include <d3d12.h>
+#include <d3dx12.h>
+
+struct ID3D12DescriptorHeap;
+
+namespace ng
+{
+	class CDX12Device;
+}
+
+namespace ng
+{
+	/*!
+	* @brief					DX12ディスクリプタヒープ
+	*/
+	class NG_DECL CDX12DescriptorHeap
+	{
+	public:
+		CDX12DescriptorHeap();
+		~CDX12DescriptorHeap();
+
+		/*!
+		* @brief					DX12ディスクリプタヒープ生成
+		* @param device				DX12デバイス
+		* @param type				ディスクリプタヒープのタイプ
+		*							D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV : コンスタントバッファビュー、シェーダーリソースビュー、アンオーダードアクセスビュー
+		*							D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER : サンプラー
+		*							D3D12_DESCRIPTOR_HEAP_TYPE_RTV : レンダーターゲットビュー
+		*							D3D12_DESCRIPTOR_HEAP_TYPE_DSV : 深度ステンシルビュー
+		* @param descriptorNum		ディスクリプターの数
+		* @param flags				オプション
+		*							D3D12_DESCRIPTOR_HEAP_FLAG_NONE	: デフォルト
+		*							D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE : シェーダーから参照する場合に指定
+		* @return					NG エラーコード
+		*/
+		NG_ERRCODE Create(
+			CDX12Device& device,
+			D3D12_DESCRIPTOR_HEAP_TYPE type,
+			u32 descriptorNum,
+			D3D12_DESCRIPTOR_HEAP_FLAGS flags
+			);
+
+		/*!
+		* @brief					破棄
+		*/
+		void Destroy();
+		
+		/*!
+		* @brief					有効か
+		*/
+		bool IsValid() const;
+
+		/*!
+		* @brief					CPUディスクリプタヒープへのハンドルを取得
+		*/
+		D3D12_CPU_DESCRIPTOR_HANDLE GetCPUDescriptorHandleForHeapStart() const;
+
+		/*!
+		* @brief					CPUディスクリプタヒープへのハンドルを取得
+		* @param index				ディスクリプタヒープ内のインデックス
+		*/
+		CD3DX12_CPU_DESCRIPTOR_HANDLE GetCPUDescriptorHandle(int index) const;
+
+		/*!
+		* @brief					GPUディスクリプタヒープへのハンドルを取得
+		*/
+		D3D12_GPU_DESCRIPTOR_HANDLE GetGPUDescriptorHandleForHeapStart() const;
+
+		/*!
+		* @brief					GPUディスクリプタヒープへのハンドルを取得
+		* @param index				ディスクリプタヒープ内のインデックス
+		*/
+		CD3DX12_GPU_DESCRIPTOR_HANDLE GetGPUDescriptorHandle(int index) const;
+
+		/*!
+		* @brief					DX12ディスクリプタヒープ インターフェース取得
+		*/
+		ID3D12DescriptorHeap* Interface();
+		const ID3D12DescriptorHeap* Interface() const;
+
+	private:
+		ID3D12DescriptorHeap* m_pIDescriptorHeap;	//!< DX12ディスクリプタヒープ インタフェース
+		u32 m_incrementSize;	//!< ディスクリプタヒープハンドルのインクリメントサイズ
+	};
+
+}	// namespace ng
+
+#endif	// __NG_GRAPHIC_DX12_DESCRIPTOR_HEAP_H__
