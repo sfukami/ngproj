f4455192830c4a39e871dc19a5dfa2ad2e078847
sfukami <xxx@dummy.com>
2020-06-24 12:35:09 +0900

    DirectX12デバッグレイヤー有効機能追加

---
+++ ngLibCore/dx/ngDXDebug.cpp
@@ -0,0 +1,26 @@
+﻿/*!
+* @file		ngDXDebug.cpp
+* @brief	DirectXデバッグ
+* @date		2017-08-23
+* @author	s.fukami
+*/
+
+#include "ngDXDebug.h"
+
+#include <d3d12.h>
+#include "ngLibCore/common/ngCommon.h"
+#include "ngDXMacro.h"
+
+namespace ng
+{
+	NG_DECL void EnableDX12DebugLayer()
+	{
+		ID3D12Debug* pIDebug = nullptr;
+
+		if(NG_SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&pIDebug)))) {
+			pIDebug->EnableDebugLayer();
+			NG_SAFE_RELEASE(pIDebug);
+		}
+	}
+
+}	// namespace ng
---
+++ ngLibCore/dx/ngDXDebug.h
@@ -0,0 +1,20 @@
+﻿/*!
+* @file		ngDXDebug.h
+* @brief	DirectXデバッグ
+* @date		2017-08-23
+* @author	s.fukami
+*/
+
+#ifndef __NG_CORE_DX_DEBUG_H__
+#define __NG_CORE_DX_DEBUG_H__
+
+namespace ng
+{
+	/*!
+	* @brief					DirectX12デバッグレイヤー有効化
+	*/
+	NG_DECL void EnableDX12DebugLayer();
+
+}	// namespace ng
+
+#endif	// __NG_CORE_DX_DEBUG_H__
