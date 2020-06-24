318f9b71c6552935f2d40e9f0e4dc9eb6d51a357
sfukami <xxx@dummy.com>
2020-06-24 17:11:06 +0900

    DX12フェンス追加

---
+++ ngLibGraphic/graphic/dx12/fence/ngDX12Fence.cpp
@@ -0,0 +1,72 @@
+﻿/*!
+* @file		ngDX12Fence.cpp
+* @brief	DX12フェンス
+* @date		2017-09-24
+* @author	s.fukami
+*/
+
+#include <d3d12.h>
+#include "ngLibCore/common/ngCommon.h"
+#include "../common/ngDX12Common.h"
+#include "../device/ngDX12Device.h"
+#include "ngDX12Fence.h"
+
+namespace ng
+{
+	CDX12Fence::CDX12Fence()
+		: m_pIFence(nullptr)
+	{
+	}
+
+	CDX12Fence::~CDX12Fence()
+	{
+		Destroy();
+	}
+
+	NG_ERRCODE CDX12Fence::Create(CDX12Device& device, D3D12_FENCE_FLAGS flags)
+	{
+		NG_ERRCODE ret = NG_ERRCODE_DEFAULT;
+		
+		// DX12フェンス生成
+		if(NG_FAILED(ret = device.Interface()->CreateFence(
+			0,
+			flags,
+			IID_PPV_ARGS(&m_pIFence)
+			))) {
+			NG_DXERR("DX12Fence", ret, CreateFence, "DX12フェンスの生成に失敗しました.");
+			return ret;
+		}
+
+		return ret;
+	}
+
+	u64 CDX12Fence::GetCompletedValue()
+	{
+		return m_pIFence->GetCompletedValue();
+	}
+
+	NG_ERRCODE CDX12Fence::SetEventOnCompletion(u64 value, HANDLE hEvent)
+	{
+		return m_pIFence->SetEventOnCompletion(value, hEvent);
+	}
+	
+	void CDX12Fence::Destroy()
+	{
+		NG_SAFE_RELEASE(m_pIFence);
+	}
+
+	bool CDX12Fence::IsValid() const
+	{
+		return (m_pIFence != nullptr);
+	}
+
+	ID3D12Fence* CDX12Fence::Interface()
+	{
+		return m_pIFence;
+	}
+	const ID3D12Fence* CDX12Fence::Interface() const
+	{
+		return m_pIFence;
+	}
+
+}	// namespace ng
---
+++ ngLibGraphic/graphic/dx12/fence/ngDX12Fence.h
@@ -0,0 +1,71 @@
+﻿/*!
+* @file		ngDX12Fence.h
+* @brief	DX12フェンス
+* @date		2017-09-24
+* @author	s.fukami
+*/
+
+#ifndef __NG_GRAPHIC_DX12_FENCE_H__
+#define __NG_GRAPHIC_DX12_FENCE_H__
+
+struct ID3D12Fence;
+
+namespace ng
+{
+	class CDX12Device;
+}
+
+namespace ng
+{
+	/*!
+	* @brief					DX12フェンス
+	*/
+	class NG_DECL CDX12Fence
+	{
+	public:
+		CDX12Fence();
+		~CDX12Fence();
+
+		/*!
+		* @brief					DX12フェンス生成
+		* @param device				DX12デバイス
+		* @param flags				フェンスオプション
+		* @return					NG エラーコード
+		*/
+		NG_ERRCODE Create(CDX12Device& device, D3D12_FENCE_FLAGS flags = D3D12_FENCE_FLAG_NONE);
+		
+		/*!
+		* @brief					現在のフェンス値を取得
+		*/
+		u64 GetCompletedValue();
+
+		/*!
+		* @brief					指定のフェンス値となった際にシグナル状態となるイベントを設定
+		* @param value				シグナル状態となるフェンス値
+		* @param hEvent				イベントハンドラ
+		*/
+		NG_ERRCODE SetEventOnCompletion(u64 value, HANDLE hEvent);
+
+		/*!
+		* @brief					破棄
+		*/
+		void Destroy();
+		
+		/*!
+		* @brief					有効か
+		*/
+		bool IsValid() const;
+		
+		/*!
+		* @brief					DX12フェンス インターフェース取得
+		*/
+		ID3D12Fence* Interface();
+		const ID3D12Fence* Interface() const;
+
+	private:
+		ID3D12Fence* m_pIFence;	//!< DX12フェンス インターフェース
+	};
+
+}	// namespace ng
+
+#endif	// __NG_GRAPHIC_DX12_FENCE_H__
