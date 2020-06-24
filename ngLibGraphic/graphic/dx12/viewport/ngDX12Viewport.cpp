4959bf2270975cfcecd5f6834d7518b6991fc01e
sfukami <xxx@dummy.com>
2020-06-24 19:32:59 +0900

    DX12ビューポート追加

---
+++ ngLibGraphic/graphic/dx12/viewport/ngDX12Viewport.cpp
@@ -0,0 +1,43 @@
+﻿/*!
+* @file		ngDX12Viewport.cpp
+* @brief	DX12ビューポート
+* @date		2018-01-20
+* @author	s.fukami
+*/
+
+#include "ngDX12Viewport.h"
+
+namespace ng
+{
+	CDX12Viewport::CDX12Viewport()
+	{
+		m_viewport = {};
+	}
+
+	CDX12Viewport::~CDX12Viewport()
+	{
+	}
+
+	void CDX12Viewport::Initialize(
+		float topLeftX,
+		float topLeftY,
+		float width,
+		float height,
+		float minDepth,
+		float maxDepth
+		)
+	{
+		m_viewport.TopLeftX = topLeftX;
+		m_viewport.TopLeftY = topLeftY;
+		m_viewport.Width = width;
+		m_viewport.Height = height;
+		m_viewport.MinDepth = minDepth;
+		m_viewport.MaxDepth = maxDepth;
+	}
+
+	const D3D12_VIEWPORT& CDX12Viewport::GetViewport() const
+	{
+		return m_viewport;
+	}
+
+}	// namespace ng
---
+++ ngLibGraphic/graphic/dx12/viewport/ngDX12Viewport.h
@@ -0,0 +1,53 @@
+﻿/*!
+* @file		ngDX12Viewport.h
+* @brief	DX12ビューポート
+* @date		2018-01-20
+* @author	s.fukami
+*/
+
+#ifndef __NG_GRAPHIC_DX12_VIEWPORT_H__
+#define __NG_GRAPHIC_DX12_VIEWPORT_H__
+
+#include <d3d12.h>
+
+namespace ng
+{
+	/*!
+	* @brief					DX12ビューポート
+	*/
+	class NG_DECL CDX12Viewport
+	{
+	public:
+		CDX12Viewport();
+		~CDX12Viewport();
+
+		/*!
+		* @param				初期化
+		* @param topLeftX		ビューポート左上X座標
+		* @param topLeftY		ビューポート左上Y座標
+		* @param width			ビューポート幅
+		* @param height			ビューポート高さ
+		* @param minDepth		ビューポート深度最小値[0,1]
+		* @param maxDepth		ビューポート深度最大値[0,1]
+		*/
+		void Initialize(
+			float topLeftX,
+			float topLeftY,
+			float width,
+			float height,
+			float minDepth,
+			float maxDepth
+			);
+		
+		/*!
+		* @param				DX12ビューポート取得
+		*/
+		const D3D12_VIEWPORT& GetViewport() const;
+
+	private:
+		D3D12_VIEWPORT m_viewport;	//!< DX12ビューポート
+	};
+
+}	// namespace ng
+
+#endif	// __NG_GRAPHIC_DX12_VIEWPORT_H__
