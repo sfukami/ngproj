93268d97b78b9c965003a5744039c66952af0ac6
sfukami <xxx@dummy.com>
2020-06-24 19:34:32 +0900

    DX12シザー矩形追加

---
+++ ngLibGraphic/graphic/dx12/scissor/ngDX12Scissor.cpp
@@ -0,0 +1,35 @@
+﻿/*!
+* @file		ngDX12Scissor.cpp
+* @brief	DX12シザー矩形
+* @date		2018-01-20
+* @author	s.fukami
+*/
+
+#include "ngDX12Scissor.h"
+
+namespace ng
+{
+	CDX12Scissor::CDX12Scissor()
+	{
+	}
+
+	CDX12Scissor::~CDX12Scissor()
+	{
+	}
+
+	void CDX12Scissor::Initialize(
+		LONG left,
+		LONG top,
+		LONG right,
+		LONG bottom
+		)
+	{
+		m_rect = CD3DX12_RECT(left, top, right, bottom);
+	}
+
+	const CD3DX12_RECT& CDX12Scissor::GetScissorRect() const
+	{
+		return m_rect;
+	}
+
+}	// namespace ng
---
+++ ngLibGraphic/graphic/dx12/scissor/ngDX12Scissor.h
@@ -0,0 +1,49 @@
+﻿/*!
+* @file		ngDX12Scissor.h
+* @brief	DX12シザー矩形
+* @date		2018-01-20
+* @author	s.fukami
+*/
+
+#ifndef __NG_GRAPHIC_DX12_SCISSOR_H__
+#define __NG_GRAPHIC_DX12_SCISSOR_H__
+
+#include <d3dx12.h>
+
+namespace ng
+{
+	/*!
+	* @brief					DX12シザー矩形
+	*/
+	class NG_DECL CDX12Scissor
+	{
+	public:
+		CDX12Scissor();
+		~CDX12Scissor();
+
+		/*!
+		* @param				初期化
+		* @param left			シザー矩形左辺
+		* @param top			シザー矩形上辺
+		* @param right			シザー矩形右辺
+		* @param bottom			シザー矩形下辺
+		*/
+		void Initialize(
+			LONG left,
+			LONG top,
+			LONG right,
+			LONG bottom
+			);
+		
+		/*!
+		* @param				DX12シザー矩形取得
+		*/
+		const CD3DX12_RECT& GetScissorRect() const;
+
+	private:
+		CD3DX12_RECT m_rect;	//!< DX12矩形
+	};
+
+}	// namespace ng
+
+#endif	// __NG_GRAPHIC_DX12_SCISSOR_H__
