6c5b53ded79a847254d030e87902ddc1601d33d7
sfukami <xxx@dummy.com>
2020-06-25 22:05:20 +0900

    DX12ヘッダ追加

---
+++ ngLibGraphic/graphic/dx12/ngDX12.h
@@ -0,0 +1,24 @@
+﻿/*!
+* @file		ngDX12.h
+* @brief	DX12 ヘッダ
+* @date		2020-06-25
+* @author	s.fukami
+*/
+
+#ifndef __NG_GRAPHIC_DX12_H__
+#define __NG_GRAPHIC_DX12_H__
+
+#include "ngDX12Util.h"
+#include "device/ngDX12Device.h"
+#include "command/allocator/ngDX12CommandAllocator.h"
+#include "command/list/ngDX12CommandList.h"
+#include "command/queue/ngDX12CommandQueue.h"
+#include "command/queue/ngDX12CommandQueueType.h"
+#include "swapchain/ngDX12SwapChain.h"
+#include "target/render/ngDX12RenderTarget.h"
+#include "target/render/ngDX12RenderTargetId.h"
+#include "target/depth/ngDX12DepthStencil.h"
+#include "target/depth/ngDX12DepthStencilId.h"
+#include "barrier/ngDX12ResourceBarrierTransition.h"
+
+#endif	// __NG_GRAPHIC_DX12_H__
