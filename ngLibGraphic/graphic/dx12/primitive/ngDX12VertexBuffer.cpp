a712c4e98d5dfa3e21a54d971e25283c836fc107
sfukami <xxx@dummy.com>
2020-06-27 13:03:13 +0900

    DX12頂点バッファ追加

---
+++ ngLibGraphic/graphic/dx12/primitive/ngDX12VertexBuffer.cpp
@@ -0,0 +1,82 @@
+﻿/*!
+* @file		ngDX12VertexBuffer.cpp
+* @brief	DX12頂点バッファ
+* @date		2018-01-20
+* @author	s.fukami
+*/
+
+#include "ngLibCore/common/ngCommon.h"
+#include "../common/ngDX12Common.h"
+#include "ngDX12VertexBuffer.h"
+
+namespace ng
+{
+	CDX12VertexBuffer::CDX12VertexBuffer()
+		: m_vertexNum(0)
+	{
+	}
+
+	CDX12VertexBuffer::~CDX12VertexBuffer()
+	{
+		Destroy();
+	}
+
+	NG_ERRCODE CDX12VertexBuffer::Create(
+		CDX12Device& device,
+		const void* pVertices,
+		u32 size,
+		u32 stride
+		)
+	{
+		NG_ASSERT(!IsValid());
+
+		NG_ERRCODE ret = NG_ERRCODE_DEFAULT;
+
+		// プリミティブ描画用リソースを生成
+		if(NG_FAILED(ret = m_resource.CreatePrimitiveBuffer(
+			device, pVertices, size
+			))) {
+			NG_ERRLOG_C("DX12VertexBuffer", ret, "DX12頂点バッファの生成に失敗しました.");
+			return ret;
+		}
+
+		m_vertexNum = size/stride;
+
+		// 頂点バッファビュー記述を設定
+		m_view.BufferLocation = m_resource.Interface()->GetGPUVirtualAddress();
+		m_view.StrideInBytes = stride;
+		m_view.SizeInBytes = size;
+
+		return ret;
+	}
+
+	void CDX12VertexBuffer::Destroy()
+	{
+		m_resource.Destroy();
+	}
+
+	bool CDX12VertexBuffer::IsValid() const
+	{
+		return m_resource.IsValid();
+	}
+
+	CDX12Resource& CDX12VertexBuffer::GetResource()
+	{
+		return m_resource;
+	}
+	const CDX12Resource& CDX12VertexBuffer::GetResource() const
+	{
+		return m_resource;
+	}
+
+	const D3D12_VERTEX_BUFFER_VIEW& CDX12VertexBuffer::GetVertexBufferView() const
+	{
+		return m_view;
+	}
+
+	u32 CDX12VertexBuffer::GetVertexNum() const
+	{
+		return m_vertexNum;
+	}
+
+}	// namespace ng
---
+++ ngLibGraphic/graphic/dx12/primitive/ngDX12VertexBuffer.h
@@ -0,0 +1,79 @@
+﻿/*!
+* @file		ngDX12VertexBuffer.h
+* @brief	DX12頂点バッファ
+* @date		2018-01-20
+* @author	s.fukami
+*/
+
+#ifndef __NG_GRAPHIC_DX12_VERTEX_BUFFER_H__
+#define __NG_GRAPHIC_DX12_VERTEX_BUFFER_H__
+
+#include <d3d12.h>
+#include "../resource/ngDX12Resource.h"
+
+namespace ng
+{
+	class CDX12Device;
+}
+
+namespace ng
+{
+	/*!
+	* @brief					DX12頂点バッファ
+	*/
+	class NG_DECL CDX12VertexBuffer
+	{
+	public:
+		CDX12VertexBuffer();
+		~CDX12VertexBuffer();
+
+		/*!
+		* @brief					DX12頂点バッファ生成
+		* @param device				DX12デバイス
+		* @param pVertices			頂点データ
+		* @param size				頂点データサイズ
+		* @param stride				頂点データ間隔のサイズ
+		* @return					NG エラーコード
+		*/
+		NG_ERRCODE Create(
+			CDX12Device& device,
+			const void* pVertices,
+			u32 size,
+			u32 stride
+			);
+
+		/*!
+		* @brief					破棄
+		*/
+		void Destroy();
+
+		/*!
+		* @brief					有効か
+		*/
+		bool IsValid() const;
+		
+		/*!
+		* @brief					DX12リソース取得
+		*/
+		CDX12Resource& GetResource();
+		const CDX12Resource& GetResource() const;
+
+		/*!
+		* @brief					頂点バッファビュー記述を取得
+		*/
+		const D3D12_VERTEX_BUFFER_VIEW& GetVertexBufferView() const;
+		
+		/*!
+		* @brief					頂点数取得
+		*/
+		u32 GetVertexNum() const;
+
+	private:
+		CDX12Resource m_resource;	//!< DX12リソース
+		D3D12_VERTEX_BUFFER_VIEW m_view;	//!< 頂点バッファビュー記述
+		u32 m_vertexNum;		//!< 頂点数
+	};
+
+}	// namespace ng
+
+#endif	// __NG_GRAPHIC_DX12_VERTEX_BUFFER_H__
