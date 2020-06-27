93e777a8d3f210c1fdc60f66947d3e6da804b1e5
sfukami <xxx@dummy.com>
2020-06-27 13:20:25 +0900

    DX12ルートシグネチャ追加

---
+++ ngLibGraphic/graphic/dx12/rootsign/ngDX12RootSignature.cpp
@@ -0,0 +1,83 @@
+﻿/*!
+* @file		ngDX12RootSignature.cpp
+* @brief	DX12ルートシグネチャ
+* @date		2017-09-24
+* @author	s.fukami
+*/
+
+#include <d3d12.h>
+#include <d3dx12.h>	// for CD3DX12_ROOT_SIGNATURE_DESC
+#include "ngLibCore/common/ngCommon.h"
+#include "../common/ngDX12Common.h"
+#include "../device/ngDX12Device.h"
+#include "ngDX12RootSignature.h"
+
+namespace ng
+{
+	CDX12RootSignature::CDX12RootSignature()
+		: m_pISignature(nullptr)
+	{
+	}
+
+	CDX12RootSignature::~CDX12RootSignature()
+	{
+		Destroy();
+	}
+
+	NG_ERRCODE CDX12RootSignature::Create(CDX12Device& device, const CD3DX12_ROOT_SIGNATURE_DESC& desc)
+	{
+		NG_ASSERT(!IsValid());
+
+		NG_ERRCODE ret = NG_ERRCODE_DEFAULT;
+
+		ID3D12Device* pIDevice = device.Interface();
+		ID3DBlob* pIBuffer = nullptr;
+		ID3DBlob* pIError = nullptr;
+
+		// ルートシグネチャ生成のためのバッファを取得
+		if(NG_FAILED(ret = D3D12SerializeRootSignature(
+			&desc,
+			D3D_ROOT_SIGNATURE_VERSION_1,
+			&pIBuffer,
+			&pIError
+			))) {
+			NG_DXERRLOG("DX12RootSignature", ret, D3D12SerializeRootSignature, "バッファの取得に失敗しました. pIError:%s", 
+				pIError ? PointerCast<const char*>(pIError->GetBufferPointer()) : "none"
+				);
+			return ret;
+		}
+
+		// ルートシグネチャ生成
+		if(NG_FAILED(ret = pIDevice->CreateRootSignature(
+			0,
+			pIBuffer->GetBufferPointer(),
+			pIBuffer->GetBufferSize(),
+			IID_PPV_ARGS(&m_pISignature)
+			))) {
+			NG_DXERRLOG("DX12RootSignature", ret, CreateRootSignature, "ルートシグネチャの生成に失敗しました.");
+			return ret;
+		}
+
+		return ret;
+	}
+
+	void CDX12RootSignature::Destroy()
+	{
+		NG_SAFE_RELEASE(m_pISignature);
+	}
+	
+	bool CDX12RootSignature::IsValid() const
+	{
+		return (m_pISignature != nullptr);
+	}
+
+	ID3D12RootSignature* CDX12RootSignature::Interface()
+	{
+		return m_pISignature;
+	}
+	const ID3D12RootSignature* CDX12RootSignature::Interface() const
+	{
+		return m_pISignature;
+	}
+
+}	// namespace ng
---
+++ ngLibGraphic/graphic/dx12/rootsign/ngDX12RootSignature.h
@@ -0,0 +1,55 @@
+﻿/*!
+* @file		ngDX12RootSignature.h
+* @brief	DX12ルートシグネチャ
+* @date		2017-09-24
+* @author	s.fukami
+*/
+
+struct ID3D12RootSignature;
+struct CD3DX12_ROOT_SIGNATURE_DESC;
+
+namespace ng
+{
+	class CDX12Device;
+}
+
+namespace ng
+{
+	/*!
+	* @brief					DX12ルートシグネチャ
+	*/
+	class NG_DECL CDX12RootSignature
+	{
+	public:
+		CDX12RootSignature();
+		~CDX12RootSignature();
+
+		/*!
+		* @brief					DX12ルートシグネチャ生成
+		* @param device				DX12デバイス
+		* @param desc				DX12ルートシグネチャ記述子
+		* @return					NG エラーコード
+		*/
+		NG_ERRCODE Create(CDX12Device& device, const CD3DX12_ROOT_SIGNATURE_DESC& desc);
+
+		/*!
+		* @brief					破棄
+		*/
+		void Destroy();
+		
+		/*!
+		* @brief					有効か
+		*/
+		bool IsValid() const;
+
+		/*!
+		* @brief					DX12ルートシグネチャ インターフェース取得
+		*/
+		ID3D12RootSignature* Interface();
+		const ID3D12RootSignature* Interface() const;
+
+	private:
+		ID3D12RootSignature* m_pISignature;	//!< DX12ルートシグネチャ インターフェース
+	};
+
+}	// namespace ng
