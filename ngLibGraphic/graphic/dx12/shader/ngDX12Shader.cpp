c6d47117a475ebaa8443833b39cec4c837a888aa
sfukami <xxx@dummy.com>
2020-06-27 13:20:01 +0900

    DX12シェーダー追加

---
+++ ngLibGraphic/graphic/dx12/shader/ngDX12Shader.cpp
@@ -0,0 +1,89 @@
+﻿/*!
+* @file		ngDX12Shader.cpp
+* @brief	DX12シェーダー
+* @date		2017-09-25
+* @author	s.fukami
+*/
+
+#include <d3d12.h>
+#include <d3dx12.h>
+#include <D3Dcompiler.h>
+#include "ngLibCore/common/ngCommon.h"
+#include "../common/ngDX12Common.h"
+#include "ngDX12Shader.h"
+
+namespace ng
+{
+	CDX12Shader::CDX12Shader()
+		: m_pIBlob(nullptr)
+	{
+	}
+
+	CDX12Shader::~CDX12Shader()
+	{
+		Destroy();
+	}
+
+	NG_ERRCODE CDX12Shader::Create(
+		const wchar_t* pFilename,
+		const char* pEntryPoint,
+		const char* pTarget,
+		u32 flags,
+		bool isDebugCompile
+		)
+	{
+		NG_ASSERT(!IsValid());
+
+		NG_ERRCODE ret = NG_ERRCODE_DEFAULT;
+
+	#if defined(NG_DEBUG)
+		// デバッグコンパイルオプション
+		// デバッグ時はシェーダーデバッグ有効、最適化無効
+		if(isDebugCompile) {
+			flags |= (D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION);
+		}
+	#endif
+
+		// シェーダーコンパイル
+		ID3DBlob* pIError = nullptr;
+		if(NG_FAILED(ret = D3DCompileFromFile(
+			pFilename,
+			nullptr,
+			nullptr,
+			pEntryPoint,
+			pTarget,
+			flags,
+			0,
+			&m_pIBlob,
+			&pIError
+			))) {
+			NG_DXERRLOG("DX12Shader", ret, D3DCompileFromFile, "シェーダーのコンパイルに失敗しました. pFilename:%ls, pEntryPoint:%s, pTarget:%s, pIError:%s", 
+				pFilename, pEntryPoint, pTarget,
+				pIError ? PointerCast<const char*>(pIError->GetBufferPointer()) : "none"
+				);
+			return ret;
+		}
+		
+		return ret;
+	}
+
+	bool CDX12Shader::IsValid() const
+	{
+		return (m_pIBlob != nullptr);
+	}
+
+	void CDX12Shader::Destroy()
+	{
+		NG_SAFE_RELEASE(m_pIBlob);
+	}
+	
+	ID3DBlob* CDX12Shader::Interface()
+	{
+		return m_pIBlob;
+	}
+	const ID3DBlob* CDX12Shader::Interface() const
+	{
+		return m_pIBlob;
+	}
+
+}	// namespace ng
---
+++ ngLibGraphic/graphic/dx12/shader/ngDX12Shader.h
@@ -0,0 +1,67 @@
+﻿/*!
+* @file		ngDX12Shader.h
+* @brief	DX12シェーダー
+* @date		2017-09-25
+* @author	s.fukami
+*/
+
+#ifndef __NG_GRAPHIC_DX12_SHADER_H__
+#define __NG_GRAPHIC_DX12_SHADER_H__
+
+struct ID3D10Blob;	// ID3DBlob
+
+namespace ng
+{
+	/*!
+	* @brief					DX12シェーダー
+	*/
+	class NG_DECL CDX12Shader
+	{
+	public:
+		/*! コンストラクタ */
+		CDX12Shader();
+
+		/*! デストラクタ */
+		virtual ~CDX12Shader();
+
+		/*!
+		* @brief					DX12シェーダー生成
+		* @param pFilename			ファイル名
+		* @param pEntryPoint		シェーダーのエントリポイント
+		* @param pTarget			シェーダーの機能レベル
+		* @param flags				コンパイルフラグ
+		* @param isDebugCompile		デバッグコンパイルオプションが有効か
+		*							NG_DEBUG 未定義時は無視される
+		* @return					NG エラーコード
+		*/
+		NG_ERRCODE Create(
+			const wchar_t* pFilename,
+			const char* pEntryPoint,
+			const char* pTarget,
+			u32 flags,
+			bool isDebugCompile
+			);
+
+		/*!
+		* @brief					破棄
+		*/
+		void Destroy();
+		
+		/*!
+		* @brief					有効か
+		*/
+		bool IsValid() const;
+
+		/*!
+		* @brief					DX12ブロブ インターフェース取得
+		*/
+		ID3D10Blob* Interface();
+		const ID3D10Blob* Interface() const;
+
+	private:
+		ID3D10Blob* m_pIBlob;	//!< DX12ブロブ インターフェース
+	};
+
+}	// namespace ng
+
+#endif	// __NG_GRAPHIC_DX12_SHADER_H__
