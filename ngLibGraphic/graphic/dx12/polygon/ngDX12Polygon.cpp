7c3c0ef61981b97b2cbe2717de10ba9d8afce9c0
sfukami <xxx@dummy.com>
2020-06-27 13:04:26 +0900

    DX12ポリゴン追加

---
+++ ngLibGraphic/graphic/dx12/polygon/ngDX12Polygon.cpp
@@ -0,0 +1,89 @@
+﻿/*!
+* @file		ngDX12Polygon.cpp
+* @brief	DX12ポリゴン
+* @date		2018-07-27
+* @author	s.fukami
+*/
+
+#include <d3dx12.h>
+#include "ngLibCore/common/ngCommon.h"
+#include "../common/ngDX12Common.h"
+#include "../command/list/ngDX12CommandList.h"
+#include "ngDX12Polygon.h"
+
+namespace ng
+{
+	CDX12Polygon::CDX12Polygon()
+		: m_primitiveTopology(D3D_PRIMITIVE_TOPOLOGY_UNDEFINED)
+	{
+	}
+
+	CDX12Polygon::~CDX12Polygon()
+	{
+		Destroy();
+	}
+
+	NG_ERRCODE CDX12Polygon::Create(
+		CDX12Device& device,
+		const void* pVertices,
+		u32 vertexDataSize,
+		u32 vertexStride,
+		D3D_PRIMITIVE_TOPOLOGY primitiveTopology,
+		const u32* pIndices,
+		u32 indexDataSize
+		)
+	{
+		NG_ASSERT(!IsValid());
+
+		NG_ERRCODE ret = m_vb.Create(
+			device, pVertices, vertexDataSize, vertexStride
+			);
+		if(NG_FAILED(ret)) {
+			return ret;
+		}
+
+		if(pIndices != nullptr) {
+			ret = m_ib.Create(
+				device, pIndices, indexDataSize
+				);
+			if(NG_FAILED(ret)) {
+				return ret;
+			}
+		}
+
+		m_primitiveTopology = primitiveTopology;
+
+		return ret;
+	}
+
+	void CDX12Polygon::Render(
+		CDX12CommandList& commandList
+		) const
+	{
+		NG_ASSERT(IsValid());
+
+		CDX12CommandList::DX12IfType* pIf = commandList.Interface();
+
+		pIf->IASetPrimitiveTopology(m_primitiveTopology);
+		pIf->IASetVertexBuffers(0, 1, &m_vb.GetVertexBufferView());
+		if(m_ib.IsValid()) {
+			pIf->IASetIndexBuffer(&m_ib.GetIndexBufferView());
+			pIf->DrawIndexedInstanced(m_ib.GetIndexNum(), 1, 0, 0, 0);
+		}
+		else {
+			pIf->DrawInstanced(m_vb.GetVertexNum(), 1, 0, 0);
+		}
+	}
+
+	void CDX12Polygon::Destroy()
+	{
+		m_ib.Destroy();
+		m_vb.Destroy();
+	}
+
+	bool CDX12Polygon::IsValid() const
+	{
+		return m_vb.IsValid() && m_ib.IsValid();
+	}
+
+}	// namespace ng
---
+++ ngLibGraphic/graphic/dx12/polygon/ngDX12Polygon.h
@@ -0,0 +1,79 @@
+﻿/*!
+* @file		ngDX12Polygon.h
+* @brief	DX12ポリゴン
+* @date		2018-07-27
+* @author	s.fukami
+*/
+
+#ifndef __NG_GRAPHIC_DX12_POLYGON_H__
+#define __NG_GRAPHIC_DX12_POLYGON_H__
+
+#include <d3dcommon.h>
+#include "../primitive/ngDX12VertexBuffer.h"
+#include "../primitive/ngDX12IndexBuffer.h"
+
+namespace ng
+{
+	class CDX12Device;
+	class CDX12CommandList;
+}
+
+namespace ng
+{
+	/*!
+	* @brief					DX12ポリゴン
+	*/
+	class NG_DECL CDX12Polygon
+	{
+	public:
+		CDX12Polygon();
+		~CDX12Polygon();
+
+		/*!
+		* @brief					DX12頂点バッファ生成
+		* @param device				DX12デバイス
+		* @param pVertices			頂点データ
+		* @param vertexDataSize		頂点データサイズ
+		* @param vertexStride		頂点データ間隔のサイズ
+		* @param primitiveTopology	プリミティブトポロジ
+		* @param pIndices			インデックスデータ
+		* @param indexDataSize		インデックスデータサイズ
+		* @return					NG エラーコード
+		*/
+		NG_ERRCODE Create(
+			CDX12Device& device,
+			const void* pVertices,
+			u32 vertexDataSize,
+			u32 vertexStride,
+			D3D_PRIMITIVE_TOPOLOGY primitiveTopology,
+			const u32* pIndices = nullptr,
+			u32 indexDataSize = 0
+			);
+
+		/*!
+		* @brief					描画
+		* @param commandList		DX12コマンドリスト
+		*/
+		void Render(
+			CDX12CommandList& commandList
+			) const;
+
+		/*!
+		* @brief					破棄
+		*/
+		void Destroy();
+
+		/*!
+		* @brief					有効か
+		*/
+		bool IsValid() const;
+		
+	private:
+		D3D_PRIMITIVE_TOPOLOGY m_primitiveTopology;	//!< プリミティブトポロジ
+		CDX12VertexBuffer m_vb;	//!< DX12頂点バッファ
+		CDX12IndexBuffer m_ib;	//!< DX12インデックスバッファ
+	};
+
+}	// namespace ng
+
+#endif	// __NG_GRAPHIC_DX12_POLYGON_H__
