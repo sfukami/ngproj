145669fabb0da93c9377090169c237c852af2f3a
sfukami <xxx@dummy.com>
2020-06-27 13:03:46 +0900

    DX12インデックスバッファ追加

---
+++ ngLibGraphic/graphic/dx12/primitive/ngDX12IndexBuffer.cpp
@@ -0,0 +1,81 @@
+﻿/*!
+* @file		ngDX12IndexBuffer.cpp
+* @brief	DX12インデックスバッファ
+* @date		2019-07-25
+* @author	s.fukami
+*/
+
+#include "ngLibCore/common/ngCommon.h"
+#include "../common/ngDX12Common.h"
+#include "ngDX12IndexBuffer.h"
+
+namespace ng
+{
+	CDX12IndexBuffer::CDX12IndexBuffer()
+		: m_indexNum(0)
+	{
+	}
+
+	CDX12IndexBuffer::~CDX12IndexBuffer()
+	{
+		Destroy();
+	}
+
+	NG_ERRCODE CDX12IndexBuffer::Create(
+		CDX12Device& device,
+		const void* pIndices,
+		u32 size
+		)
+	{
+		NG_ASSERT(!IsValid());
+
+		NG_ERRCODE ret = NG_ERRCODE_DEFAULT;
+
+		// プリミティブ描画用リソースを生成
+		if(NG_FAILED(ret = m_resource.CreatePrimitiveBuffer(
+			device, pIndices, size
+			))) {
+			NG_ERRLOG_C("DX12IndexBuffer", ret, "DX12インデックスバッファの生成に失敗しました.");
+			return ret;
+		}
+
+		m_indexNum = size/sizeof(u32);
+
+		// 頂点バッファビュー記述を設定
+		m_view.BufferLocation = m_resource.Interface()->GetGPUVirtualAddress();
+		m_view.SizeInBytes = size;
+		m_view.Format = DXGI_FORMAT_R32_UINT;
+
+		return ret;
+	}
+
+	void CDX12IndexBuffer::Destroy()
+	{
+		m_resource.Destroy();
+	}
+
+	bool CDX12IndexBuffer::IsValid() const
+	{
+		return m_resource.IsValid();
+	}
+
+	CDX12Resource& CDX12IndexBuffer::GetResource()
+	{
+		return m_resource;
+	}
+	const CDX12Resource& CDX12IndexBuffer::GetResource() const
+	{
+		return m_resource;
+	}
+
+	const D3D12_INDEX_BUFFER_VIEW& CDX12IndexBuffer::GetIndexBufferView() const
+	{
+		return m_view;
+	}
+
+	u32 CDX12IndexBuffer::GetIndexNum() const
+	{
+		return m_indexNum;
+	}
+
+}	// namespace ng
---
+++ ngLibGraphic/graphic/dx12/primitive/ngDX12IndexBuffer.h
@@ -0,0 +1,77 @@
+﻿/*!
+* @file		ngDX12IndexBuffer.h
+* @brief	DX12インデックスバッファ
+* @date		2019-07-25
+* @author	s.fukami
+*/
+
+#ifndef __NG_GRAPHIC_DX12_INDEX_BUFFER_H__
+#define __NG_GRAPHIC_DX12_INDEX_BUFFER_H__
+
+#include <d3d12.h>
+#include "../resource/ngDX12Resource.h"
+
+namespace ng
+{
+	class CDX12Device;
+}
+
+namespace ng
+{
+	/*!
+	* @brief					DX12インデックスバッファ
+	*/
+	class NG_DECL CDX12IndexBuffer
+	{
+	public:
+		CDX12IndexBuffer();
+		~CDX12IndexBuffer();
+
+		/*!
+		* @brief					DX12インデックスバッファ生成
+		* @param device				DX12デバイス
+		* @param pIndices			インデックスデータ
+		* @param size				インデックスデータサイズ
+		* @return					NG エラーコード
+		*/
+		NG_ERRCODE Create(
+			CDX12Device& device,
+			const void* pIndices,
+			u32 size
+			);
+
+		/*!
+		* @brief					破棄
+		*/
+		void Destroy();
+
+		/*!
+		* @brief					有効か
+		*/
+		bool IsValid() const;
+
+		/*!
+		* @brief					DX12リソース取得
+		*/
+		CDX12Resource& GetResource();
+		const CDX12Resource& GetResource() const;
+
+		/*!
+		* @brief					インデックスバッファビュー記述を取得
+		*/
+		const D3D12_INDEX_BUFFER_VIEW& GetIndexBufferView() const;
+
+		/*!
+		* @brief					インデックス数取得
+		*/
+		u32 GetIndexNum() const;
+
+	private:
+		CDX12Resource m_resource;	//!< DX12リソース
+		D3D12_INDEX_BUFFER_VIEW m_view;	//!< インデックスバッファビュー記述
+		u32 m_indexNum;		//!< インデックス数
+	};
+	
+}	// namespace ng
+
+#endif	// __NG_GRAPHIC_DX12_INDEX_BUFFER_H__
