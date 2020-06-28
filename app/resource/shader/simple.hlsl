067974e862e78eb2f175bdf316cea1f52325fe50
sfukami <xxx@dummy.com>
2020-06-29 02:14:51 +0900

    仮シェーダー追加

---
+++ app/resource/shader/simple.hlsl
@@ -0,0 +1,49 @@
+/*!
+* @file		simple.hlsl
+* @brief	簡易シェーダー
+* @date		2019-08-31
+* @author	s.fukami
+*/
+
+// --------------------------------
+// ConstantBuffer
+// --------------------------------
+cbuffer ShaderParameter : register(b0)
+{
+	float4x4 wvp;
+	float4 color;
+};
+
+// --------------------------------
+// VS Input/Output
+// --------------------------------
+struct VSInput
+{
+	float4 pos			: POSITION;
+	float4 normal		: NORMAL;
+	float2 uv			: TEXCOORD0;
+};
+struct VSOutput
+{
+	float4 pos			: SV_POSITION;
+};
+
+// --------------------------------
+// VertexShader
+// --------------------------------
+VSOutput VSMain(VSInput In)
+{
+	VSOutput result = (VSOutput)0;
+
+	result.pos = mul(In.pos, wvp);
+
+	return result;
+}
+
+// --------------------------------
+// PixelShader
+// --------------------------------
+float4 PSMain(VSOutput In) : SV_TARGET
+{
+	return color;
+}
