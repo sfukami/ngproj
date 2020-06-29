c226de7916ead3abeca8f923a5b77f9689b91869
sfukami <xxx@dummy.com>
2020-06-29 21:13:27 +0900

    カメラ追加

---
+++ ngLibGraphic/camera/ngCamera.cpp
@@ -0,0 +1,109 @@
+﻿/*!
+* @file		ngCamera.cpp
+* @brief	カメラ
+* @date		2019-08-07
+* @author	s.fukami
+*/
+
+#include "ngLibCore/geometry/vector/ngVectorOp.h"
+#include "ngLibCore/geometry/matrix/ngMatrixOp.h"
+#include "ngCamera.h"
+
+namespace ng
+{
+	CCamera::CCamera()
+		: m_pos(0, 0, 0)
+		, m_at(0, 0, -1)
+		, m_up(0, 1, 0)
+		, m_view(Matrix4::IDENTITY)
+		, m_coordSys(eCoordSys::LEFT_HAND)
+	{
+	}
+	CCamera::~CCamera()
+	{
+	}
+
+	void CCamera::CalcViewMatrix()
+	{
+		if(m_coordSys == eCoordSys::LEFT_HAND) {
+			MatrixOp::ViewLH(m_view, m_pos, m_at, m_up);
+		}
+		else if(m_coordSys == eCoordSys::RIGHT_HAND) {
+			MatrixOp::ViewRH(m_view, m_pos, m_at, m_up);
+		}
+	}
+
+	void CCamera::SetPos(const Vector3& pos)
+	{
+		m_pos = pos;
+	}
+	const Vector3& CCamera::GetPos() const
+	{
+		return m_pos;
+	}
+
+	void CCamera::SetAtPos(const Vector3& at)
+	{
+		m_at = at;
+	}
+	const Vector3& CCamera::GetAtPos() const
+	{
+		return m_at;
+	}
+
+	void CCamera::SetUpVec(const Vector3& up)
+	{
+		m_up = up;
+	}
+	const Vector3& CCamera::GetUpVec() const
+	{
+		return m_up;
+	}
+	
+
+	void CCamera::MoveStraightToAtPos(float length)
+	{
+		Vector3 dir;
+		VectorOp::Normalize(dir, m_at-m_pos);
+		m_pos += dir * length;
+	}
+
+	void CCamera::RotateAroundAtPos(float radH, float radV)
+	{
+		Vector3 toAt = m_at-m_pos;
+		// TODO: 回転に違和感.ワールドX軸周りの回転とする
+		/*
+		// カメラのX方向の軸を算出
+		Vector3 side;
+		VectorOp::Cross(side, toAt, m_up);
+		side.y = 0;
+		VectorOp::Normalize(side, side);
+		*/
+
+		Matrix4 rotHMat, rotVMat;
+		MatrixOp::RotationAxis(rotHMat, Vector3::AXIS_Y, radH);
+		//MatrixOp::RotationAxis(rotVMat, side, radV);
+		MatrixOp::RotationAxis(rotVMat, Vector3::AXIS_X, radV);
+		MatrixOp::Multiply(rotHMat, rotHMat, rotVMat);
+
+		VectorOp::Transform(toAt, toAt, rotHMat);
+		VectorOp::Transform(m_up, m_up, rotHMat);
+
+		m_pos = m_at - toAt;
+	}
+
+	const Matrix4& CCamera::GetViewMatrix() const
+	{
+		return m_view;
+	}
+
+	void CCamera::SetCoordSys(eCoordSys coordSys)
+	{
+		m_coordSys = coordSys;
+	}
+	eCoordSys CCamera::GetCoordSys() const
+	{
+		return m_coordSys;
+	}
+
+}	// namespace ng
---
+++ ngLibGraphic/camera/ngCamera.h
@@ -0,0 +1,73 @@
+﻿/*!
+* @file		ngCamera.h
+* @brief	カメラ
+* @date		2019-08-07
+* @author	s.fukami
+*/
+
+#ifndef __NG_GRAPHIC_CAMERA_H__
+#define __NG_GRAPHIC_CAMERA_H__
+
+#include "ngLibCore/geometry/vector/ngVector3.h"
+#include "ngLibCore/geometry/matrix/ngMatrix4x4.h"
+#include "ngLibGraphic/coordsys/ngCoordSys.h"
+
+namespace ng
+{
+	/*!
+	* @brief					カメラ
+	*/
+	class NG_DECL CCamera
+	{
+	public:
+		CCamera();
+		~CCamera();
+
+		/*!
+		* @brief					ビュー行列算出
+		*/
+		void CalcViewMatrix();
+
+		/*! 座標 */
+		void SetPos(const Vector3& pos);
+		const Vector3& GetPos() const;
+
+		/*! 注視点座標 */
+		void SetAtPos(const Vector3& at);
+		const Vector3& GetAtPos() const;
+
+		/*! 上方ベクトル */
+		void SetUpVec(const Vector3& up);
+		const Vector3& GetUpVec() const;
+
+		/*!
+		* @brief					注視点と現座標の直線上を移動
+		* @param length				移動距離
+		*/
+		void MoveStraightToAtPos(float length);
+
+		/*!
+		* @brief					注視点を中心に回転
+		* @param rh					水平方向回転角（ラジアン）
+		* @param rv					垂直方向回転角（ラジアン）
+		*/
+		void RotateAroundAtPos(float radH, float radV);
+
+		/*! ビュー行列 */
+		const Matrix4& GetViewMatrix() const;
+
+		/*! 座標系 */
+		void SetCoordSys(eCoordSys coordSys);
+		eCoordSys GetCoordSys() const;
+		
+	private:
+		Vector3 m_pos;		//!< 座標
+		Vector3 m_at;		//!< 注視点座標
+		Vector3 m_up;		//!< 上方ベクトル
+		Matrix4 m_view;		//!< ビュー行列
+		eCoordSys m_coordSys;	//!< 座標系
+	};
+
+}	// namespace ng
+
+#endif	// __NG_GRAPHIC_CAMERA_H__
