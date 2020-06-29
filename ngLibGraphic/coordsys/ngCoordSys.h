35e501d4cb0d633b2faf60e0893d0089d1a6243d
sfukami <xxx@dummy.com>
2020-06-29 21:10:59 +0900

    座標系定義追加

---
+++ ngLibGraphic/coordsys/ngCoordSys.h
@@ -0,0 +1,24 @@
+﻿/*!
+* @file		ngCoordSys.h
+* @brief	座標系
+* @date		2019-08-07
+* @author	s.fukami
+*/
+
+#ifndef __NG_GRAPHIC_COORDSYS_H__
+#define __NG_GRAPHIC_COORDSYS_H__
+
+namespace ng
+{
+	/*!
+	* @brief					座標系
+	*/
+	enum class eCoordSys : u32
+	{
+		LEFT_HAND,		//!< 左手座標系
+		RIGHT_HAND,		//!< 右手座標系
+	};
+
+}	// namespace ng
+
+#endif	// __NG_GRAPHIC_COORDSYS_H__
