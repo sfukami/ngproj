0ac4fdc29d8303417934d93b622fc6e36729b286
sfukami <xxx@dummy.com>
2020-06-29 21:14:13 +0900

    プロジェクション追加

---
+++ ngLibGraphic/projection/ngProjection.cpp
@@ -0,0 +1,143 @@
+﻿/*!
+* @file		ngProjection.cpp
+* @brief	射影変換
+* @date		2019-08-07
+* @author	s.fukami
+*/
+
+#include "ngLibCore/math/ngMath.h"
+#include "ngLibCore/geometry/matrix/ngMatrixOp.h"
+#include "ngProjection.h"
+
+namespace ng
+{
+	CProjection::CProjection()
+		: m_width(480)
+		, m_height(640)
+		, m_near(0.1f)
+		, m_far(100.f)
+		, m_fov(DegreeToRadian(45.f))
+		, m_proj(Matrix4::IDENTITY)
+		, m_coordSys(eCoordSys::LEFT_HAND)
+		, m_projMethod(PERSPECTIVE)
+	{
+
+	}
+	CProjection::~CProjection()
+	{
+
+	}
+
+	void CProjection::CalcProjMatrix()
+	{
+		if(m_projMethod == PERSPECTIVE) {
+			if(m_coordSys == eCoordSys::LEFT_HAND) {
+				MatrixOp::ProjPersLH(
+					m_proj,
+					m_fov,
+					m_width / (f32)m_height,
+					m_near,
+					m_far
+					);
+			}
+			else if(m_coordSys == eCoordSys::RIGHT_HAND) {
+				MatrixOp::ProjPersRH(
+					m_proj,
+					m_fov,
+					m_width / (f32)m_height,
+					m_near,
+					m_far
+					);
+			}
+		}
+		else if(m_projMethod == ORTHOGONAL) {
+			if(m_coordSys == eCoordSys::LEFT_HAND) {
+				MatrixOp::ProjOrthoLH(
+					m_proj,
+					(f32)m_width,
+					(f32)m_height,
+					m_near,
+					m_far
+					);
+			}
+			else if(m_coordSys == eCoordSys::RIGHT_HAND) {
+				MatrixOp::ProjOrthoRH(
+					m_proj,
+					(f32)m_width,
+					(f32)m_height,
+					m_near,
+					m_far
+					);
+			}
+		}
+	}
+
+	void CProjection::SetWidth(int width)
+	{
+		m_width = width;
+	}
+	int CProjection::GetWidth() const
+	{
+		return m_width;
+	}
+
+	void CProjection::SetHeight(int height)
+	{
+		m_height = height;
+	}
+	int CProjection::GetHeight() const
+	{
+		return m_height;
+	}
+
+	void CProjection::SetNear(float _near)
+	{
+		m_near = _near;
+	}
+	float CProjection::GetNear() const
+	{
+		return m_near;
+	}
+
+	void CProjection::SetFar(float _far)
+	{
+		m_far = _far;
+	}
+	float CProjection::GetFar() const
+	{
+		return m_far;
+	}
+
+	void CProjection::SetFOV(float rad)
+	{
+		m_fov = rad;
+	}
+	float CProjection::GetFOV() const
+	{
+		return m_fov;
+	}
+
+	const Matrix4& CProjection::GetProjMatrix() const
+	{
+		return m_proj;
+	}
+
+	void CProjection::SetCoordSys(eCoordSys coordSys)
+	{
+		m_coordSys = coordSys;
+	}
+	eCoordSys CProjection::GetCoordSys() const
+	{
+		return m_coordSys;
+	}
+
+	void CProjection::SetProjMethod(eProjMethod projMethod)
+	{
+		m_projMethod = projMethod;
+	}
+	CProjection::eProjMethod CProjection::GetProjMethod() const
+	{
+		return m_projMethod;
+	}
+
+}	// namespace ng
---
+++ ngLibGraphic/projection/ngProjection.h
@@ -0,0 +1,82 @@
+﻿/*!
+* @file		ngProjection.h
+* @brief	射影変換
+* @date		2019-08-07
+* @author	s.fukami
+*/
+
+#ifndef __NG_GRAPHIC_PROJECTION_H__
+#define __NG_GRAPHIC_PROJECTION_H__
+
+#include "ngLibCore/geometry/matrix/ngMatrix4x4.h"
+#include "ngLibGraphic/coordsys/ngCoordSys.h"
+
+namespace ng
+{
+	/*!
+	* @brief					射影変換
+	*/
+	class NG_DECL CProjection
+	{
+	public:
+		/*! 投影方法 */
+		enum eProjMethod
+		{
+			PERSPECTIVE,	//!< 透視投影
+			ORTHOGONAL,		//!< 平行投影
+		};
+
+	public:
+		CProjection();
+		~CProjection();
+
+		/*!
+		* @brief					プロジェクション行列算出
+		*/
+		void CalcProjMatrix();
+
+		/*! スクリーン幅 */
+		void SetWidth(int width);
+		int GetWidth() const;
+
+		/*! スクリーン高さ */
+		void SetHeight(int height);
+		int GetHeight() const;
+
+		/*! ニア面 */
+		void SetNear(float _near);
+		float GetNear() const;
+
+		/*! ファー面 */
+		void SetFar(float _far);
+		float GetFar() const;
+
+		/*! 視野角 */
+		void SetFOV(float rad);
+		float GetFOV() const;
+
+		/*! プロジェクション行列取得 */
+		const Matrix4& GetProjMatrix() const;
+
+		/*! 座標系 */
+		void SetCoordSys(eCoordSys coordSys);
+		eCoordSys GetCoordSys() const;
+
+		/*! 投影方法 */
+		void SetProjMethod(eProjMethod projMethod);
+		eProjMethod GetProjMethod() const;
+		
+	private:
+		int m_width;	//!< スクリーン幅
+		int m_height;	//!< スクリーン高さ
+		float m_near;	//!< ニア面
+		float m_far;	//!< ファー面
+		float m_fov;	//!< 視野角
+		Matrix4 m_proj;	//!< プロジェクション行列
+		eCoordSys m_coordSys;	//!< 座標系
+		eProjMethod m_projMethod;	//!< 投影方法
+	};
+
+}	// namespace ng
+
+#endif	// __NG_GRAPHIC_PROJECTION_H__
