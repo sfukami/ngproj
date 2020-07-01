3a254345ba8a2559ce5e3dcc801fd56e6a8c223c
sfukami <xxx@dummy.com>
2020-07-01 16:12:18 +0900

    グラフィックパイプライン モデル描画仮追加

---
+++ app/graphic/pipeline/test/appGraphicPipelineModel.cpp
@@ -0,0 +1,66 @@
+﻿/*!
+* @file		appGraphicPipelineModel.cpp
+* @brief	グラフィックパイプライン バッファクリア
+* @date		2020-06-24
+* @author	s.fukami
+*/
+
+#include "ngLibGraphic/graphic/ngGraphicManager.h"
+#include "ngLibGraphic/graphic/dx12/ngDX12.h"
+#include "appGraphicPipelineModel.h"
+
+namespace app
+{
+	CGraphicPipelineModel::CGraphicPipelineModel()
+	{
+	}
+	CGraphicPipelineModel::~CGraphicPipelineModel()
+	{
+	}
+
+	bool CGraphicPipelineModel::_initialize()
+	{
+		const unsigned int clientWidth = 640;
+		const unsigned int clientHeight = 480;
+
+		m_viewport.Initialize(0.0f, 0.0f, (float)clientWidth, (float)clientHeight, 0.0f, 1.0f);
+		m_scissor.Initialize(0, 0, (LONG)clientWidth, (LONG)clientHeight);
+
+		return true;
+	}
+
+	void CGraphicPipelineModel::_finalize()
+	{
+
+	}
+
+	void CGraphicPipelineModel::_execute()
+	{
+		// コマンドリストリセット
+		ng::CDX12CommandList* pCmdList = ng::DX12Util::GetCommandList(0);
+		pCmdList->Reset();
+
+		// ビューポート設定
+		pCmdList->SetViewport(m_viewport);
+		// シザー矩形設定
+		pCmdList->SetScissorRect(m_scissor);
+
+		// バックバッファをレンダリングターゲットとして使用
+		ng::CDX12RenderTarget* pRTBackBuffer = ng::DX12Util::GetRenderTargetOfCurrentBackBuffer();
+		ng::CDX12DepthStencil* pDS = ng::DX12Util::GetDepthStencil(ng::eDX12DepthStencilId::ID_01);
+		ng::DX12Util::SetRenderTarget(pCmdList, pRTBackBuffer, pDS);
+
+		// バックバッファクリア
+		const float clearColor[4] = {0.0f, 0.0f, 0.5f, 1.0f};
+		ng::DX12Util::ClearRenderTarget(pCmdList, pRTBackBuffer, clearColor, pDS);
+
+		// 描画コマンド実行
+		ng::CGraphicManager::GetInstance().Render();
+
+		ng::DX12Util::SetRenderTargetToPresent(pCmdList, pRTBackBuffer);
+
+		// コマンドの記録を終了
+		pCmdList->Close();
+	}
+
+}	// namespace app
---
+++ app/graphic/pipeline/test/appGraphicPipelineModel.h
@@ -0,0 +1,50 @@
+﻿/*!
+* @file		appGraphicPipelineModel.h
+* @brief	グラフィックパイプライン モデル描画
+* @date		2020-06-24
+* @author	s.fukami
+*/
+
+#ifndef __APP_GRAPHIC_PIPELINE_MODEL_H__
+#define __APP_GRAPHIC_PIPELINE_MODEL_H__
+
+#include "../appGraphicPipeline.h"
+#include "ngLibGraphic/graphic/dx12/viewport/ngDX12Viewport.h"
+#include "ngLibGraphic/graphic/dx12/scissor/ngDX12Scissor.h"
+
+namespace app
+{
+	/*!
+	* @brief					グラフィックパイプライン モデル描画
+	*/
+	class CGraphicPipelineModel : public CGraphicPipeline
+	{
+	public:
+		CGraphicPipelineModel();
+		~CGraphicPipelineModel();
+
+	private:
+		/*!
+		* @brief					初期化
+		* @return					成否
+		*/
+		bool _initialize();
+
+		/*!
+		* @brief					終了処理
+		*/
+		void _finalize();
+
+		/*!
+		* @brief					実行
+		*/
+		void _execute();
+		
+	private:
+		ng::CDX12Viewport m_viewport;	//!< DX12ビューポート
+		ng::CDX12Scissor m_scissor;		//!< DX12シザー矩形
+	};
+
+}	// namespace app
+
+#endif	// __APP_GRAPHIC_PIPELINE_MODEL_H__
