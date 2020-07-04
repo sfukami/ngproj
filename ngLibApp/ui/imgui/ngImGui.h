f8849c1ab3cbc4271b3bedd342e9612dbb1aae46
sfukami <xxx@dummy.com>
2020-07-04 16:37:36 +0900

    [ImGui] 必要そうなソースファイルを追加

---
+++ ngLibApp/ui/imgui/ngImGui.h
@@ -0,0 +1,24 @@
+﻿/*!
+* @file		ngImGui.h
+* @brief	ImGui ラッパー
+* @date		2020-07-04
+* @author	s.fukami
+*/
+
+#ifndef __NG_APP_IMGUI_H__
+#define __NG_APP_IMGUI_H__
+
+namespace ng
+{
+	namespace ImGui
+	{
+
+		/*!
+		* @brief					ImGuiセットアップ
+		*/
+		void SetupImGui();
+
+	}	// namespace ImGui
+}	// namespace ng
+
+#endif	// __NG_APP_IMGUI_H__
