9c4f2c485e673572919f7daba434b9d9844e4126
sfukami <xxx@dummy.com>
2020-07-04 12:48:59 +0900

    インスタンス化不可追加

---
+++ ngLibCore/traits/ngNonInstantiable.h
@@ -0,0 +1,27 @@
+﻿/*!
+* @file		ngNonInstantiable.h
+* @brief	インスタンス化禁止
+* @date		2020-07-03
+* @author	s.fukami
+*/
+
+#ifndef __NG_CORE_NON_INSTANTIABLE_H__
+#define __NG_CORE_NON_INSTANTIABLE_H__
+
+namespace ng
+{
+	/*!
+	* @brief					インスタンス化禁止
+	* @note						継承して使用する
+	*/
+	class NG_DECL CNonInstantiable
+	{
+	public:
+		CNonInstantiable() = delete;
+		CNonInstantiable(const CNonInstantiable&) = delete;
+		CNonInstantiable(CNonInstantiable&&) = delete;
+	};
+
+}	// namespace ng
+
+#endif	// __NG_CORE_NON_INSTANTIABLE_H__
