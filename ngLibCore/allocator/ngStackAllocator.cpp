ff598c2596c30a0f01636aef7a5240b774e798c5
sfukami <xxx@dummy.com>
2020-07-04 13:50:44 +0900

    StackAllocator追加

---
+++ ngLibCore/allocator/ngStackAllocator.cpp
@@ -0,0 +1,158 @@
+﻿/*!
+* @file		ngStackAllocator.cpp
+* @brief	スタックアロケータ
+* @date		2017-02-26
+* @author	s.fukami
+*/
+
+#include "ngLibCore/common/ngCommon.h"
+#include "ngStackAllocator.h"
+
+namespace ng
+{
+	CStackAllocator::CStackAllocator()
+		: m_pBeg(nullptr)
+		, m_pEnd(nullptr)
+		, m_pPos(nullptr)
+		, m_pAlloc(nullptr)
+		, m_name("")
+	{
+	}
+
+	CStackAllocator::~CStackAllocator()
+	{
+		Finalize();
+	}
+
+	NG_ERRCODE CStackAllocator::Initialize(
+		const char* name
+		, IMemoryAllocator& alloc
+		, size_type size
+		)
+	{
+		NG_ERRCODE ret = _initialize(name, NG_NEW_BYTE(alloc, size), size);
+		if(NG_SUCCEEDED(ret)) {
+			m_pAlloc = &alloc;
+		}
+		return ret;
+	}
+
+	NG_ERRCODE CStackAllocator::Initialize(
+		const char* name
+		, CMemoryPool& pool
+		, size_type size
+		)
+	{
+		return _initialize(name, pool.GetMemory(), size);
+	}
+
+	void CStackAllocator::Finalize()
+	{
+		if(!IsInit()) return;
+
+		void* pMemory = GetMemory();
+
+		NG_CLRSTR(m_name);
+		m_pBeg = m_pEnd = m_pPos = nullptr;
+		m_memPool.Unpool();
+
+		if(m_pAlloc) {
+			NG_DELETE_BYTE(*m_pAlloc, pMemory);
+			m_pAlloc = nullptr;
+		}
+	}
+
+	void* CStackAllocator::Allocate(
+		size_type size
+		, u32 alignment
+	#if defined(NG_CONFIG_MEMORY_DBGINFO)
+		, const char* src
+	#endif
+		)
+	{
+		if(!IsInit()) return nullptr;
+
+		char* pNext = PointerOffset<char*>(m_pPos, size);
+		if(pNext >= m_pEnd) {
+			/*
+			NG_DPRINTF("メモリ領域の割り当てに失敗。"
+				#if defined(NG_CONFIG_MEMORY_DBGINFO)
+				"DEGINFO[src:%s]", src
+				#endif
+				);
+			*/
+			return nullptr;
+		}
+
+		char* pPrev = m_pPos;
+		m_pPos = pNext;
+
+		return pPrev;
+	}
+
+	void CStackAllocator::Free(
+		void* p
+		)
+	{
+		if(!IsInit()) return;
+
+		NG_ASSERT(p >= m_pBeg);
+		NG_ASSERT(p < m_pEnd);
+
+		m_pPos = static_cast<char*>(p);
+	}
+
+	void CStackAllocator::Clear()
+	{
+		m_pPos = m_pBeg;
+	}
+
+	bool CStackAllocator::IsInit() const
+	{
+		return (GetMemory() != nullptr);
+	}
+	
+	void* CStackAllocator::GetMemory()
+	{
+		return m_pBeg;
+	}
+	const void* CStackAllocator::GetMemory() const
+	{
+		return m_pBeg;
+	}
+
+	size_type CStackAllocator::GetMemSize() const
+	{
+		return PointerDiff(m_pEnd, m_pBeg);
+	}
+
+	size_type CStackAllocator::GetAllocatableMaximum() const
+	{
+		return PointerDiff(m_pEnd, m_pPos);
+	}
+
+	const char* CStackAllocator::GetName() const
+	{
+		return m_name;
+	}
+
+	NG_ERRCODE CStackAllocator::_initialize(const char* name, void* pMemory, size_type size)
+	{
+		NG_ASSERT(!IsInit(), "不正な再初期化");
+		NG_ASSERT_NOT_NULL(name);
+		NG_ASSERT_NOT_NULL(pMemory);
+
+		NG_ERRCODE ret = m_memPool.Pool(pMemory, size);
+		if(NG_FAILED(ret)) {
+			return ret;
+		}
+
+		m_pBeg = m_pPos = static_cast<char*>(m_memPool.GetMemory());
+		m_pEnd = PointerOffset<char*>(m_pBeg, size);
+
+		NG_STRCPY(m_name, name);
+
+		return ret;
+	}
+
+}	// namespace ng
---
+++ ngLibCore/allocator/ngStackAllocator.h
@@ -0,0 +1,131 @@
+﻿/*!
+* @file		ngStackAllocator.h
+* @brief	スタックアロケータ
+* @date		2017-02-26
+* @author	s.fukami
+*/
+
+#ifndef __NG_CORE_STACK_ALLOCATOR_H__
+#define __NG_CORE_STACK_ALLOCATOR_H__
+
+#include "ngMemoryAllocator.h"
+#include "ngLibCore/memory/pool/ngMemoryPool.h"
+
+namespace ng
+{
+	/*!
+	* @brief					スタックアロケータ
+	*/
+	class NG_DECL CStackAllocator : public IMemoryAllocator
+	{
+	public:
+		CStackAllocator();
+		~CStackAllocator();
+
+		/*!
+		* @brief					初期化
+		* @param name				メモリアロケータの名称
+		* @param alloc				使用するメモリアロケータ
+		* @param size				使用するメモリ領域サイズ（バイト）
+		* @return					NG エラーコード
+		*/
+		NG_ERRCODE Initialize(
+			const char* name
+			, IMemoryAllocator& alloc
+			, size_type size
+			);
+
+		/*!
+		* @brief					初期化
+		* @param name				メモリアロケータの名称
+		* @param pool				使用するメモリプール
+		* @param size				使用するメモリ領域サイズ（バイト）
+		* @return					NG エラーコード
+		*/
+		NG_ERRCODE Initialize(
+			const char* name
+			, CMemoryPool& pool
+			, size_type size
+			);
+
+		/*!
+		* @brief					終了処理
+		*/
+		void Finalize();
+
+		/*!
+		* @brief					メモリ領域を割り当て、取得する
+		* @param size				割り当てるメモリ領域サイズ（バイト）
+		* @param alignment			アライメント（バイト）
+		* @param src				割り当て元の情報（NG_CONF_MEMORY_DBGINFO定義時のみ）
+		* @return					割り当てたメモリ領域へのポインタ
+		* @attention				アライメントは無効
+		*/
+		void* Allocate(
+			size_type size
+			, u32 alignment
+		#if defined(NG_CONFIG_MEMORY_DBGINFO)
+			, const char* src
+		#endif
+			);
+
+		/*!
+		* @brief					割り当て済みのメモリ領域を解放する
+		* @param p					解放するメモリ領域へのポインタ
+		* @attention				オブジェクトの解放処理は行わないので注意
+		*/
+		void Free(
+			void* p
+			);
+
+		/*!
+		* @brief					全メモリ解放
+		* @attention				個々のオブジェクトの解放処理は行わないので注意
+		*/
+		void Clear();
+
+		/*!
+		* @brief					初期化済みか
+		*/
+		bool IsInit() const;
+
+		/*!
+		* @brief					メモリ領域を取得する
+		*/
+		void* GetMemory();
+		const void* GetMemory() const;
+
+		/*!
+		* @brief					メモリ領域のサイズを取得する
+		*/
+		size_type GetMemSize() const;
+
+		/*!
+		* @brief					割り当て可能な最大サイズを取得する
+		*/
+		size_type GetAllocatableMaximum() const;
+
+		/*!
+		* @brief					名称取得
+		*/
+		const char* GetName() const;
+
+	private:
+		/*! 初期化 */
+		NG_ERRCODE _initialize(const char* name, void* pMemory, size_type size);
+
+	private:
+		static const u32 NAME_LENGTH = 127;		//!< 名称の長さ
+
+	private:
+		CMemoryPool	m_memPool;		//!< メモリプール
+		char* m_pBeg;	//!< 先頭ポインタ
+		char* m_pEnd;	//!< 末尾ポインタ
+		char* m_pPos;	//!< マーカーの位置
+		char m_name[ NAME_LENGTH+1 ];	//!< 名称
+		IMemoryAllocator*	m_pAlloc;	//!< 利用するメモリアロケータ
+	};
+
+}	// namespace ng
+
+#endif	// __NG_CORE_STACK_ALLOCATOR_H__
