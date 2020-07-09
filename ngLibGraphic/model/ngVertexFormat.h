68fc98432d7d33326fad2aa746b3ee03dd038afc
sfukami <xxx@dummy.com>
2020-07-09 11:11:43 +0900

    DX12頂点レイアウト追加

---
+++ ngLibGraphic/model/ngVertexFormat.h
@@ -0,0 +1,46 @@
+﻿/*!
+* @file		ngVertexFormat.h
+* @brief	頂点フォーマット
+* @date		2019-08-13
+* @author	s.fukami
+*/
+
+#ifndef __NG_GRAPHIC_VERTEX_FORMAT_H__
+#define __NG_GRAPHIC_VERTEX_FORMAT_H__
+
+#include "ngLibCore/geometry/ngGeometry.h"
+
+namespace ng
+{
+	namespace VertexFormat
+	{
+		/*! スプライトの頂点フォーマット */
+		struct Sprite
+		{
+			Vector3 pos;
+			Vector4 color;
+			Vector2 uv;
+		};
+
+		/*! スタティックメッシュの頂点フォーマット */
+		struct Static
+		{
+			Vector3 pos;
+			Vector3 normal;
+			Vector2 uv;
+		};
+
+		/*! スケルタルメッシュの頂点フォーマット */
+		struct Skeletal
+		{
+			Vector3 pos;
+			Vector3 normal;
+			Vector2 uv;
+			u16 bones[4];
+			f32 weights[4];
+		};
+
+	}	// namespace VertexFormat
+}	// namespace ng
+
+#endif	// __NG_GRAPHIC_VERTEX_FORMAT_H__
