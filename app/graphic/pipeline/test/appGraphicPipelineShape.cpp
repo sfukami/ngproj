cc9710406c527241f770ce83adc6373452716351
sfukami <xxx@dummy.com>
2020-07-09 11:12:23 +0900

    DX12ポリゴン矩形追加

---
+++ app/graphic/pipeline/test/appGraphicPipelineShape.cpp
@@ -0,0 +1,278 @@
+﻿/*!
+* @file		appGraphicPipelineShape.cpp
+* @brief	グラフィックパイプライン シェイプ描画
+* @date		2020-07-08
+* @author	s.fukami
+*/
+
+#include "ngLibCore/common/ngCommon.h"
+#include "ngLibCore/math/ngMath.h"
+#include "ngLibGraphic/graphic/dx12/ngDX12.h"
+#include "ngLibCore/geometry/matrix/ngMatrix4x4.h"
+#include "ngLibCore/geometry/matrix/ngMatrixOp.h"
+#include "ngLibCore/color/ngColor.h"
+#include "appGraphicPipelineShape.h"
+#include "../../appGraphicModule.h"
+
+namespace app
+{
+	//! シェーダーパラメータ
+	struct ShaderParam
+	{
+		ShaderParam()
+			: color(1,1,1,1)
+		{ ng::MatrixOp::Identity(matWVP); }
+		ng::Matrix4 matWVP;
+		ng::Color color;
+	};
+
+	CGraphicPipelineShape::CGraphicPipelineShape()
+	{
+	}
+	CGraphicPipelineShape::~CGraphicPipelineShape()
+	{
+	}
+
+	bool CGraphicPipelineShape::_initialize()
+	{
+		const unsigned int clientWidth = 640;
+		const unsigned int clientHeight = 480;
+
+		ng::CDX12Device* pDX12Device = ng::DX12Util::GetDevice();
+		NG_ASSERT(pDX12Device != nullptr);
+
+		// ルートシグネチャ生成
+		{
+			D3D12_DESCRIPTOR_RANGE descriptorRange = {};
+			descriptorRange.NumDescriptors = 1;
+			descriptorRange.BaseShaderRegister = 0;
+			descriptorRange.RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_CBV;
+			descriptorRange.OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+
+			D3D12_ROOT_PARAMETER rootParam = {};
+			rootParam.ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
+			rootParam.ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
+			rootParam.DescriptorTable.NumDescriptorRanges = 1;
+			rootParam.DescriptorTable.pDescriptorRanges = &descriptorRange;
+
+			D3D12_STATIC_SAMPLER_DESC samplerDesc = {};
+			samplerDesc.Filter = D3D12_FILTER_MIN_MAG_MIP_POINT;
+			samplerDesc.AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+			samplerDesc.AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+			samplerDesc.AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+			samplerDesc.MipLODBias = 0.0f;
+			samplerDesc.MaxAnisotropy = 16;
+			samplerDesc.ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER;
+			samplerDesc.BorderColor = D3D12_STATIC_BORDER_COLOR_TRANSPARENT_BLACK;
+			samplerDesc.MinLOD = 0.0f;
+			samplerDesc.MaxLOD = D3D12_FLOAT32_MAX;
+			samplerDesc.ShaderRegister = 0;
+			samplerDesc.RegisterSpace = 0;
+			samplerDesc.ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
+
+			CD3DX12_ROOT_SIGNATURE_DESC rootSignDesc;
+			rootSignDesc.Init(
+				1, &rootParam,
+				1, &samplerDesc,
+				D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT
+				);
+
+			NG_ERRCODE err = m_rootSign.Create(*pDX12Device, rootSignDesc);
+			if(NG_FAILED(err)) {
+				NG_ERRLOG_C("GraphicPipelineShape", err, "DX12ルートシグネチャの生成に失敗しました.");
+				return false;
+			}
+		}
+
+		// ディスクリプタヒープ生成
+		{
+			NG_ERRCODE err = m_descHeap.Create(
+				*pDX12Device,
+				D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
+				1,
+				D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE
+				);
+			if(NG_FAILED(err)) {
+				NG_ERRLOG_C("GraphicPipelineShape", err, "DX12ディスクリプタヒープの生成に失敗しました.");
+				return false;
+			}
+		}
+
+		// コンスタントバッファ生成
+		{
+			NG_ERRCODE err = m_constBuf.Create(
+				*pDX12Device,
+				nullptr, sizeof(ShaderParam)
+				);
+			if(NG_FAILED(err)) {
+				NG_ERRLOG_C("GraphicPipelineShape", err, "DX12コンスタントバッファの生成に失敗しました.");
+				return false;
+			}
+
+			// リソースビュー生成
+			m_constBuf.CreateResourceView(
+				*pDX12Device, m_descHeap, 0
+				);
+		}
+
+		// シェーダー生成
+		{
+			NG_ERRCODE err = NG_ERRCODE_DEFAULT;
+			if(NG_FAILED(err = m_vs.Create(
+				L"resource/shader/simple.hlsl",
+				"VSMain",
+				"vs_5_0",
+				0, true
+				))) {
+				NG_ERRLOG_C("GraphicPipelineShape", err, "DX12頂点シェーダーの生成に失敗しました.");
+				return false;
+			}
+			if(NG_FAILED(err = m_ps.Create(
+				L"resource/shader/simple.hlsl",
+				"PSMain",
+				"ps_5_0",
+				0, true
+				))) {
+				NG_ERRLOG_C("GraphicPipelineShape", err, "DX12ピクセルシェーダーの生成に失敗しました.");
+				return false;
+			}
+		}
+
+		// DX12ポリゴン 矩形生成
+		{
+			NG_ERRCODE err = m_square.Create(*pDX12Device);
+			if(NG_FAILED(err)) {
+				NG_ERRLOG_C("GraphicPipelineShape", err, "DX12ポリゴン 矩形の生成に失敗しました.");
+				return false;
+			}
+		}
+
+		// パイプラインステート生成
+		{
+			ng::CDX12PipelineStateDesc stateDesc;
+
+			// ルートシグネチャ設定
+			stateDesc.SetRootSignature(m_rootSign);
+
+			// シェーダー設定
+			stateDesc.SetVertexShader(m_vs);
+			stateDesc.SetPixelShader(m_ps);
+
+			// 頂点入力レイアウト設定
+			D3D12_INPUT_ELEMENT_DESC inputElemDescs[] = {
+				{"POSITION",	0, DXGI_FORMAT_R32G32B32_FLOAT,		0, D3D12_APPEND_ALIGNED_ELEMENT,	D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
+				{"COLOR",		0, DXGI_FORMAT_R32G32B32A32_FLOAT,	0, D3D12_APPEND_ALIGNED_ELEMENT,	D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0}
+			};
+			stateDesc.InputLayout = {inputElemDescs, _countof(inputElemDescs)};
+
+			stateDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+			//stateDesc.RasterizerState.FrontCounterClockwise = true;
+			stateDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+			stateDesc.DepthStencilState.DepthEnable = FALSE;
+			stateDesc.DepthStencilState.StencilEnable = FALSE;
+			stateDesc.SampleMask = UINT_MAX;
+			stateDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+			stateDesc.NumRenderTargets = 1;
+			stateDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+			stateDesc.SampleDesc.Count = 1;
+
+			NG_ERRCODE err = m_pipelineState.Create(*pDX12Device, stateDesc);
+			if(NG_FAILED(err)) {
+				NG_ERRLOG_C("GraphicPipelineShape", err, "DX12パイプラインステートの生成に失敗しました.");
+				return false;
+			}
+		}
+
+		// カメラ
+		{
+			m_camera.SetPos(ng::Vector3(0.f, 0.f, -3.f));
+			m_camera.SetAtPos(ng::Vector3::ZERO);
+			m_camera.SetUpVec(ng::Vector3::AXIS_Y);
+			m_camera.CalcViewMatrix();
+		}
+
+		// プロジェクション
+		{
+			m_proj.SetWidth(clientWidth);
+			m_proj.SetHeight(clientHeight);
+			m_proj.SetNear(0.1f);
+			m_proj.SetFar(100.f);
+			m_proj.SetFOV(ng::DegreeToRadian(60.f));
+			m_proj.CalcProjMatrix();
+		}
+
+		m_viewport.Initialize(0.0f, 0.0f, (float)clientWidth, (float)clientHeight, 0.0f, 1.0f);
+		m_scissor.Initialize(0, 0, (LONG)clientWidth, (LONG)clientHeight);
+
+		return true;
+	}
+
+	void CGraphicPipelineShape::_finalize()
+	{
+		m_vs.Destroy();
+		m_ps.Destroy();
+
+		m_square.Destroy();
+
+		m_constBuf.Destroy();
+		m_descHeap.Destroy();
+
+		m_pipelineState.Destroy();
+		m_rootSign.Destroy();
+	}
+
+	void CGraphicPipelineShape::_execute()
+	{
+		// コマンドリストリセット
+		ng::CDX12CommandList* pCmdList = _getDX12CommandList(eGraphicCommandListId::MAIN);
+		pCmdList->Reset(m_pipelineState);
+
+		// ルートシグネチャ設定
+		pCmdList->SetRootSignature(m_rootSign);
+
+		// ビューポート設定
+		pCmdList->SetViewport(m_viewport);
+		// シザー矩形設定
+		pCmdList->SetScissorRect(m_scissor);
+
+		// バックバッファをレンダリングターゲットとして使用
+		ng::CDX12RenderTarget* pRTBackBuffer = ng::DX12Util::GetRenderTargetOfCurrentBackBuffer();
+		ng::CDX12DepthStencil* pDS = ng::DX12Util::GetDepthStencil(ng::eDX12DepthStencilId::ID_01);
+		ng::DX12Util::SetRenderTarget(pCmdList, pRTBackBuffer, pDS);
+
+		// バックポリゴン描画
+		const float clearColor[4] = {0.0f, 0.0f, 0.5f, 1.0f};
+		ng::DX12Util::ClearRenderTarget(pCmdList, pRTBackBuffer, clearColor, pDS);
+
+		// ポリゴン描画
+		{
+			// シェーダーパラメータ設定
+			ng::Matrix4 matWorld, matView, matProj, matWVP;
+			ng::MatrixOp::Identity(matWorld);
+			matView = m_camera.GetViewMatrix();
+			matProj = m_proj.GetProjMatrix();
+
+			ng::MatrixOp::Multiply(matWVP, matView, matProj);
+			ng::MatrixOp::Multiply(matWVP, matWorld, matWVP);
+			ng::MatrixOp::Transpose(matWVP, matWVP);
+
+			ShaderParam shdPrm;
+			shdPrm.matWVP = matWVP;
+			shdPrm.color = ng::Color(0,1,0);
+			m_constBuf.CopyData(&shdPrm, sizeof(ShaderParam));
+
+			// ディスクリプタヒープ設定
+			pCmdList->SetDescriptorHeap(m_descHeap);
+			// コンスタントバッファのディスクリプタテーブルを設定
+			pCmdList->SetGraphicsRootDescriptorTable(0, m_descHeap.GetGPUDescriptorHandle(0));
+
+			m_square.Render(*pCmdList);
+		}
+
+		ng::DX12Util::SetRenderTargetToPresent(pCmdList, pRTBackBuffer);
+
+		// コマンドの記録を終了
+		pCmdList->Close();
+	}
+
+}	// namespace app
---
+++ app/graphic/pipeline/test/appGraphicPipelineShape.h
@@ -0,0 +1,70 @@
+﻿/*!
+* @file		appGraphicPipelineShape.h
+* @brief	グラフィックパイプライン シェイプ描画
+* @date		2020-07-08
+* @author	s.fukami
+*/
+
+#ifndef __APP_GRAPHIC_PIPELINE_SHAPE_H__
+#define __APP_GRAPHIC_PIPELINE_SHAPE_H__
+
+#include "../appGraphicPipeline.h"
+#include "ngLibGraphic/graphic/dx12/shader/ngDX12Shader.h"
+#include "ngLibGraphic/graphic/dx12/rootsign/ngDX12RootSignature.h"
+#include "ngLibGraphic/graphic/dx12/pipeline/ngDX12PipelineState.h"
+#include "ngLibGraphic/graphic/dx12/descriptor/ngDX12DescriptorHeap.h"
+#include "ngLibGraphic/graphic/dx12/shader/ngDX12ConstantBuffer.h"
+#include "ngLibGraphic/graphic/dx12/viewport/ngDX12Viewport.h"
+#include "ngLibGraphic/graphic/dx12/scissor/ngDX12Scissor.h"
+#include "ngLibGraphic/camera/ngCamera.h"
+#include "ngLibGraphic/projection/ngProjection.h"
+// shape
+#include "ngLibGraphic/graphic/dx12/polygon/shape/ngDX12ShapeSquare.h"
+
+namespace app
+{
+	/*!
+	* @brief					グラフィックパイプライン シェイプ描画
+	*/
+	class CGraphicPipelineShape : public CGraphicPipeline
+	{
+	public:
+		CGraphicPipelineShape();
+		~CGraphicPipelineShape();
+
+	private:
+		/*!
+		* @brief					初期化
+		* @return					成否
+		*/
+		bool _initialize();
+
+		/*!
+		* @brief					終了処理
+		*/
+		void _finalize();
+
+		/*!
+		* @brief					実行
+		*/
+		void _execute();
+		
+	private:
+		ng::CDX12Shader m_vs;			//!< DX12頂点シェーダー
+		ng::CDX12Shader m_ps;			//!< DX12ピクセルシェーダー
+		ng::CDX12RootSignature m_rootSign;	//!< DX12ルートシグネチャ
+		ng::CDX12PipelineState m_pipelineState;	//!< DX12パイプラインステート
+		ng::CDX12DescriptorHeap m_descHeap;	//!< DX12ディスクリプタヒープ
+		ng::CDX12ConstantBuffer m_constBuf;	//!< DX12コンスタントバッファ
+		ng::CDX12Viewport m_viewport;	//!< DX12ビューポート
+		ng::CDX12Scissor m_scissor;		//!< DX12シザー矩形
+		ng::CCamera m_camera;		//!< カメラ
+		ng::CProjection m_proj;		//!< プロジェクション
+
+		// shape
+		ng::CDX12ShapeSquare m_square;	//!< DX12ポリゴン 矩形
+	};
+
+}	// namespace app
+
+#endif	// __APP_GRAPHIC_PIPELINE_SHAPE_H__
