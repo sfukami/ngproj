725559a307afda481ce63f374b506b1f71c9ee06
sfukami <xxx@dummy.com>
2020-07-13 22:39:08 +0900

    ImGuiヘッダ追加

---
+++ ngLibApp/ui/imgui/ngImGui.h
@@ -0,0 +1,13 @@
+﻿/*!
+* @file		ngImGui.h
+* @brief	ImGuiヘッダ
+* @date		2020-07-13
+* @author	s.fukami
+*/
+
+#ifndef __NG_APP_IMGUI_H__
+#define __NG_APP_IMGUI_H__
+
+#include "imgui_src/imgui.h"
+
+#endif	// __NG_APP_IMGUI_H__
