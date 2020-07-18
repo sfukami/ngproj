18177e28f999baa33b24c32e7d2e084c8c0011e0
sfukami <xxx@dummy.com>
2020-07-18 21:42:05 +0900

    ステートマシン追加

---
+++ ngLibApp/state/ngStateMachine.h
@@ -0,0 +1,154 @@
+﻿/*!
+* @file		ngStateMachine.h
+* @brief	ステートマシン
+* @date		2020-07-18
+* @author	s.fukami
+*/
+
+#ifndef __NG_APP_STATE_MACHINE_H__
+#define __NG_APP_STATE_MACHINE_H__
+
+#include "ngLibCore/container/array/ngFixedArray.h"
+#include "ngLibCore/action/ngAction.h"
+
+namespace ng
+{
+	/*!
+	* @brief					ステートマシン
+	*/
+	template <typename State, State Num, typename Process>
+	class NG_DECL CStateMachine
+	{
+	public:
+		/*!
+		* @brief					コンストラクタ
+		*/
+		CStateMachine();
+
+		/*!
+		* @brief					コンストラクタ
+		* @param defaultState		デフォルトステート
+		*/
+		CStateMachine(State defaultState);
+
+		/*!
+		* @brief					デストラクタ
+		*/
+		~CStateMachine();
+
+		/*!
+		* @brief					ステート設定
+		* @param process			ステート処理
+		*/
+		void SetState(State state, const Process&& process);
+
+		/*!
+		* @brief					ステート切り替え
+		*/
+		void ChangeState(State state);
+
+		/*!
+		* @brief					ステート処理実行
+		*/
+		void Execute();
+
+	private:
+		State m_curState;	//!< 現在のステート
+		CFixedArray<Process, static_cast<u32>(Num)> m_procArr;	//!< ステート処理の配列
+	};
+
+	template <typename State, State Num, typename Process>
+	CStateMachine<State, Num, Process>::CStateMachine()
+		: m_curState(State())
+	{
+	}
+
+	template <typename State, State Num, typename Process>
+	CStateMachine<State, Num, Process>::CStateMachine(State defaultState)
+		: m_curState(defaultState)
+	{
+	}
+
+	template <typename State, State Num, typename Process>
+	CStateMachine<State, Num, Process>::~CStateMachine()
+	{
+	}
+
+	template <typename State, State Num, typename Process>
+	void CStateMachine<State, Num, Process>::SetState(State state, const Process&& process)
+	{
+		m_procArr[static_cast<u32>(state)] = std::move(process);
+	}
+
+	template <typename State, State Num, typename Process>
+	void CStateMachine<State, Num, Process>::ChangeState(State state)
+	{
+		m_curState = state;
+	}
+
+	template <typename State, State Num, typename Process>
+	void CStateMachine<State, Num, Process>::Execute()
+	{
+		m_procArr[static_cast<u32>(m_curState)]();
+	}
+
+	/*!
+	* @brief					ステートマシン クラス用
+	*/
+	template <class T, typename State, State Num, typename Process = CClassAction<T>>
+	class NG_DECL CClassStateMachine : public CStateMachine<State, Num, Process>
+	{
+	public:
+		using BaseType = CStateMachine<State, Num, Process>;	//!< 基底クラス
+		using BaseType::SetState;
+		
+	public:
+		/*!
+		* @brief					コンストラクタ
+		*/
+		CClassStateMachine();
+
+		/*!
+		* @brief					コンストラクタ
+		* @param defaultState		デフォルトステート
+		*/
+		CClassStateMachine(State defaultState);
+
+		/*!
+		* @brief					デストラクタ
+		*/
+		~CClassStateMachine();
+
+		/*!
+		* @brief					ステート設定
+		* @param pObj				呼び出し元のオブジェクト
+		* @param pFunc				メンバ関数ポインタ
+		*/
+		void SetState(State state, T* pObj, typename Process::FuncType pFunc);
+	};
+
+	template <class T, typename State, State Num, typename Process>
+	CClassStateMachine<T, State, Num, Process>::CClassStateMachine()
+	{
+	}
+
+	template <class T, typename State, State Num, typename Process>
+	CClassStateMachine<T, State, Num, Process>::CClassStateMachine(State defaultState)
+		: BaseType(defaultState)
+	{
+	}
+
+	template <class T, typename State, State Num, typename Process>
+	CClassStateMachine<T, State, Num, Process>::~CClassStateMachine()
+	{
+	}
+
+	template <class T, typename State, State Num, typename Process>
+	void CClassStateMachine<T, State, Num, Process>::SetState(State state, T* pObj, typename Process::FuncType pFunc)
+	{
+		SetState(state, Process(pObj, pFunc));
+	}
+
+}	// namespace ng
+
+#endif	//__NG_APP_STATE_MACHINE_H__
---
+++ test/testcode/state/testStateMachine.cpp
@@ -0,0 +1,63 @@
+﻿/*!
+* @file		testStateMachine.cpp
+* @brief	テストコード ステートマシン
+* @date		2020-07-18
+* @author	s.fukami
+*/
+
+#include "ngLibCore/action/ngAction.h"
+#include "ngLibApp/state/ngStateMachine.h"
+
+namespace test
+{
+	/*!
+	* @brief					ステートマシン テスト
+	*/
+	TEST_CLASS(CTestStateMachine)
+	{
+	private:
+		class CTest
+		{
+		public:
+			//enum State
+			enum class State : unsigned int
+			{
+				FIRST, SECOND, THIRD,
+				NUM,
+				DEFAULT = SECOND,
+			};
+
+		public:
+			void First() { ng::Printf("%s\n", NG_FUNCTION); }
+			void Second() { ng::Printf("%s\n", NG_FUNCTION); }
+			void Third() { ng::Printf("%s\n", NG_FUNCTION); }
+		};
+
+	private:
+		TEST_DEFINE_INITIALIZE;
+
+		TEST_METHOD(TestStateMachine)
+		{
+			CTest t;
+
+			ng::CClassStateMachine<CTest, CTest::State, CTest::State::NUM> sm(CTest::State::DEFAULT);
+
+			// ステート設定
+			sm.SetState(CTest::State::FIRST, ng::CClassAction<CTest>(&t, &CTest::First));
+			sm.SetState(CTest::State::SECOND, &t, &CTest::Second);
+			sm.SetState(CTest::State::THIRD, &t, &CTest::Third);
+
+			// 実行
+			sm.Execute();
+
+			sm.ChangeState(CTest::State::FIRST);
+
+			sm.Execute();
+
+			sm.ChangeState(CTest::State::THIRD);
+
+			sm.Execute();
+		}
+	};
+
+}	// namespace test
