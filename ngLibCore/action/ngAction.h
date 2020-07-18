225d3e3810b55c26a62c514aefdf959ce69521c4
sfukami <xxx@dummy.com>
2020-07-18 20:28:39 +0900

    CAction追加

---
+++ ngLibCore/action/ngAction.h
@@ -0,0 +1,108 @@
+﻿/*!
+* @file		ngAction.h
+* @brief	アクション
+* @date		2020-07-18
+* @author	s.fukami
+*/
+
+#ifndef __NG_CORE_ACTION_H__
+#define __NG_CORE_ACTION_H__
+
+#include <tuple>
+#include "../tuple/ngTupleArgs.h"
+
+namespace ng
+{
+	/*!
+	* @brief					アクション
+	* @tparam Ret				戻り値
+	* @tparam Args...			引数
+	*/
+	template <typename Ret = void, typename... Args>
+	class NG_DECL CAction
+	{
+	public:
+		using FuncType = Ret(*)(Args...);	//!< 関数の型
+		using ArgsType = typename std::tuple<Args...>;	//!< 引数のタプル型
+
+	public:
+		/*!
+		* @brief					コンストラクタ
+		* @param pFunc				関数ポインタ
+		*/
+		CAction(FuncType pFunc)
+			: m_pFunc(pFunc)
+		{ }
+
+		/*!
+		* @brief					実行
+		* @param args				引数
+		*/
+		Ret operator()(Args... args)
+		{
+			return (*m_pFunc)(args...);
+		}
+
+		/*!
+		* @brief					実行
+		* @param args				引数
+		*/
+		Ret operator()(std::tuple<Args...>&& args)
+		{
+			return CallFunc(*this, args);
+		}
+
+	private:
+		FuncType m_pFunc;	//!< 関数ポインタ
+	};
+
+	/*!
+	* @brief					アクション（クラス版）
+	* @tparam T					呼び出し元のクラス
+	* @tparam Ret				戻り値
+	* @tparam Args...			引数
+	*/
+	template <class T, typename Ret = void, typename... Args>
+	class NG_DECL CClassAction
+	{
+	public:
+		using ObjType = T;	//!< オブジェクトの型
+		using FuncType = Ret(ObjType::*)(Args...);	//!< 関数の型
+		using ArgsType = typename std::tuple<Args...>;	//!< 引数のタプル型
+
+	public:
+		/*!
+		* @brief					コンストラクタ
+		* @param pObj				呼び出し元のオブジェクト
+		* @param pFunc				メンバ関数ポインタ
+		*/
+		CClassAction(ObjType* pObj, FuncType pFunc)
+			: m_pObj(pObj), m_pFunc(pFunc)
+		{ }
+
+		/*!
+		* @brief					実行
+		* @param args				引数
+		*/
+		Ret operator()(Args... args)
+		{
+			return (m_pObj->*m_pFunc)(args...);
+		}
+
+		/*!
+		* @brief					実行
+		* @param args				引数
+		*/
+		Ret operator()(std::tuple<Args...>&& args)
+		{
+			return CallFunc(*this, args);
+		}
+
+	private:
+		ObjType* m_pObj;	//!< 呼び出し元のオブジェクト
+		FuncType m_pFunc;	//!< メンバ関数ポインタ
+	};
+
+}	// namespace ng
+
+#endif	//__NG_CORE_ACTION_H__
---
+++ ngLibCore/tuple/ngTupleArgs.h
@@ -0,0 +1,49 @@
+﻿/*!
+* @file		ngTupleArgs.h
+* @brief	std::tuple の引数展開
+* @date		2020-07-18
+* @author	s.fukami
+*/
+
+#ifndef __NG_CORE_TUPLE_ARGS_H__
+#define __NG_CORE_TUPLE_ARGS_H__
+
+#include <type_traits>
+
+namespace ng
+{
+	/*!
+	* @brief					std::tuple引数関数の呼び出し実装
+	*/
+	template <typename Func, typename Tuple, bool Done, int Total, int... N>
+	struct NG_DECL CallFuncImpl
+	{
+		static void CallFunc(Func& func, Tuple&& args)
+		{
+			CallFuncImpl<Func, Tuple, Total == 1 + sizeof...(N), Total, N..., sizeof...(N)>
+				::CallFunc(func, std::forward<Tuple>(args));
+		}
+	};
+	template <typename Func, typename Tuple, int Total, int... N>
+	struct NG_DECL CallFuncImpl<Func, Tuple, true, Total, N...>
+	{
+		static void CallFunc(Func& func, Tuple&& args)
+		{
+			func(std::get<N>(std::forward<Tuple>(args))...);
+		}
+	};
+
+	/*!
+	* @brief					std::tuple引数関数の呼び出し
+	*/
+	template <typename Func, typename Tuple>
+	void CallFunc(Func& func, Tuple&& args)
+	{
+		typedef typename std::decay<Tuple>::type TupleType;
+		CallFuncImpl<Func, Tuple, 0 == std::tuple_size<TupleType>::value, std::tuple_size<TupleType>::value>
+			::CallFunc(func, std::forward<Tuple>(args));
+	}
+
+}	// namespace ng
+
+#endif	// __NG_CORE_TUPLE_ARGS_H__
