68863922e8e5baa0b2d290032a37eae50308240a
sfukami <xxx@dummy.com>
2020-07-23 00:57:01 +0900

    マテリアル仮追加

---
+++ app/graphic/render/material/appMaterialFormat.h
@@ -0,0 +1,36 @@
+﻿/*!
+* @file		appMaterialFormat.h
+* @brief	マテリアルフォーマット
+* @date		2020-07-22
+* @author	s.fukami
+*/
+
+#ifndef __APP_MATERIAL_FORMAT_H__
+#define __APP_MATERIAL_FORMAT_H__
+
+namespace app
+{
+	/*!
+	* @brief					テクスチャデータ
+	*/
+	struct TextureData
+	{
+		TextureData();
+
+		char filePath[256];	//!< ファイルパス
+		void* pBinary;		//!< バイナリへの参照
+	};
+
+	/*!
+	* @brief					マテリアルデータ
+	*/
+	struct MaterialData
+	{
+		MaterialData();
+
+		TextureData diffuseMap;	//!< ディフューズマップ
+	};
+
+}	// namespace app
+
+#endif	//__APP_MATERIAL_FORMAT_H__
