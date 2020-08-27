e94718c7a1f39ec4bd3fbcad9d5b404630fc5fa4
sfukami <xxx@dummy.com>
2020-08-28 01:16:11 +0900

    TransformObject追加

---
+++ app/transform/appTransformObject.cpp
@@ -0,0 +1,28 @@
+﻿/*!
+* @file		appTransformObject.cpp
+* @brief	トランスフォームオブジェクト
+* @date		2020-08-28
+* @author	s.fukami
+*/
+
+#include "appTransformObject.h"
+
+namespace app
+{
+	CTransformObject::CTransformObject()
+	{
+	}
+	CTransformObject::~CTransformObject()
+	{
+	}
+
+	CTransform& CTransformObject::GetTransform()
+	{
+		return m_transform;
+	}
+	const CTransform& CTransformObject::GetTransform() const
+	{
+		return m_transform;
+	}
+
+}	// namespace app
---
+++ app/transform/appTransformObject.h
@@ -0,0 +1,34 @@
+﻿/*!
+* @file		appTransformObject.h
+* @brief	トランスフォームオブジェクト
+* @date		2020-08-28
+* @author	s.fukami
+*/
+
+#ifndef __APP_TRANSFORM_OBJECT_H__
+#define __APP_TRANSFORM_OBJECT_H__
+
+#include "appTransform.h"
+
+namespace app
+{
+	/*!
+	* @brief					トランスフォームオブジェクト
+	*/
+	class CTransformObject
+	{
+	public:
+		CTransformObject();
+		virtual ~CTransformObject();
+
+		/*! トランスフォーム取得 */
+		CTransform& GetTransform();
+		const CTransform& GetTransform() const;
+		
+	private:
+		CTransform m_transform;	//!< トランスフォーム
+	};
+
+}	// namespace app
+
+#endif	// __APP_TRANSFORM_OBJECT_H__
